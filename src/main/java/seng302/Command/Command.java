8bbbbbae692488eb5e82f31afa6f07326eb0d003
Jack Steel <jes143@uclive.ac.nz>
2018-03-14 10:53:48 +1300

    Created a basic commandPattern structure for commands. - Uses the well defined command structure - Uses stacks to store undo/redo functionality - Created class for CreateUser command, has not been tested yet #implement #story[435]

---
+++ src/main/java/seng302/Command/Command.java
@@ -0,0 +1,6 @@
+package seng302.Command;
+
+public interface Command {
+    public void execute();
+    public void unExecute();
+}
---
+++ src/main/java/seng302/Command/CreateUserCommand.java
@@ -0,0 +1,33 @@
+package seng302.Command;
+
+import seng302.Donor;
+import seng302.DonorManager;
+
+import java.time.LocalDate;
+
+public class CreateUserCommand implements Command {
+
+
+    private Donor donor;
+    private DonorManager manager;
+
+
+    public CreateUserCommand (String firstName, String middleNames, String lastName, LocalDate dateOfBirth, int uid, DonorManager manager) {
+
+        Donor donor = new Donor(firstName, middleNames, lastName, dateOfBirth, uid);
+
+        this.manager = manager;
+    }
+
+
+    @Override
+    public void execute() {
+        manager.addDonor(donor);
+
+    }
+
+    @Override
+    public void unExecute() {
+        manager.removeDonor(donor);
+    }
+}
