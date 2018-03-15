1cd6a60225d910e89c73ee3d5c1cc8cae39c5090
Dylan Carlyle <dca87@uclive.ac.nz>
2018-03-15 14:28:55 +1300

    added in the create user controller class, edited the create user fxml and cleaned up some unused imports.

---
+++ src/main/java/seng302/Controller/CreateUserController.java
@@ -0,0 +1,14 @@
+package seng302.Controller;
+
+
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class CreateUserController {
+    public Button createUserBtn;
+    public DatePicker dobFld;
+    public TextField firstNameFld;
+    public TextField middleNamefld;
+    public TextField lastNamefld;
+}
