3eca757b03c95b27d502507c289fbf8ce52ef43c
Dylan Carlyle <dca87@uclive.ac.nz>
2018-03-16 13:07:33 +1300

    Added the sidebar to the History page.

--- src/main/java/seng302/Controller/HistoryController.java
+++ src/main/java/seng302/Controller/HistoryController.java
@@ -1,4 +1,29 @@
 package seng302.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import seng302.Utilities.Page;
+
+import java.io.IOException;
+
 public class HistoryController {
+
+    @FXML
+    Pane sidebarPane;
+
+    @FXML
+    private void initialize() {
+        // IMPORTING SIDEBAR //
+        try {
+            VBox sidebar = FXMLLoader.load(getClass().getResource(Page.SIDEBAR.getPath()));
+            sidebarPane.getChildren().setAll(sidebar);
+        } catch (IOException exc) {
+            System.err.println("Couldn't load sidebar from fxml file.");
+            exc.printStackTrace();
+        }
+
+    }
+
 }
