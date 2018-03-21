34d2e87a2369339ebb3617df8239169439977729
Alex Tompkins <ato47@uclive.ac.nz>
2018-03-21 21:35:49 +1300

    Removed changes to MainController from previous commit, and fixed scaling on create_donor.fxml. #fix #story[429]
    Took 11 minutes

--- src/main/java/seng302/Controller/MainController.java
+++ src/main/java/seng302/Controller/MainController.java
@@ -3,13 +3,8 @@ package seng302.Controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-
-import java.io.IOException;
 
 /**
  * Main controller class for the application window.
@@ -21,21 +16,12 @@ public class MainController {
 	@FXML
 	private StackPane pageHolder;
 
-	@FXML
-	private AnchorPane rootPane;
-
 	/**
 	 * Replaces the page displayed in the page holder with a new page.
 	 * @param node the page node to be swapped in.
 	 */
 	public void setPage(Node node) {
 		pageHolder.getChildren().setAll(node);
-	}
-
-	@FXML
-	private void loadSidebar(ActionEvent event) throws IOException {
-		VBox vbox = FXMLLoader.load(getClass().getResource("seng302.resources.sidebar.fxml"));
-		rootPane.getChildren().setAll(vbox);
 	}
 
 	/**
