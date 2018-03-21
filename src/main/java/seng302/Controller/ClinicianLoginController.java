287bfcd8de21aee2751c9d445c955ef8a5de8cbc
james <jto59@uclive.ac.nz>
2018-03-22 11:29:15 +1300

    Merge branch 's16-search-donors' of /home/cosc/student/jto59/IdeaProjects/team-21 with conflicts.

--- src/main/java/seng302/Controller/ClinicianLoginController.java
+++ src/main/java/seng302/Controller/ClinicianLoginController.java
@@ -2,14 +2,20 @@ package seng302.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import seng302.Clinician;
 import seng302.State;
 import seng302.Utilities.Page;
 import seng302.Utilities.PageNavigator;
+import sun.security.util.Password;
 
 public class ClinicianLoginController {
 
-	public TextField idTextField;
+	@FXML
+	private TextField staffID;
+	@FXML
+	private PasswordField password;
 
 
 	@FXML
@@ -17,9 +23,26 @@ public class ClinicianLoginController {
 		PageNavigator.loadPage(Page.LANDING.getPath());
 	}
 
+	private boolean validStaffIDinput() {
+		try {
+			Integer.parseInt(staffID.getText());
+		}
+	}
+
 	@FXML
 	private void signIn(ActionEvent event) {
-		State.setPageParam("currentUserId", Integer.parseInt(idTextField.getText()));
+		if (validStaffIDinput()) {
+
+		}
+
+		Clinician clinician = State.getClinicianManager().getClinicianByStaffId(staffID.getText());
+		if (clinician != null && clinician.getPassword().equals(password.getText())) {
+
+		}
+
+		//State.setPageParam("currentUserId", Integer.parseInt(idTextField.getText()));
 		State.setPageParam("currentUserType", "donor");
+
+
 		PageNavigator.loadPage(Page.VIEW_DONOR.getPath());}
 }
