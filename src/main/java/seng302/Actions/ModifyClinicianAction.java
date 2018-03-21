fb3306e39acb5798c78fbdd326f149f41ce348fe
Jack Steel <jes143@uclive.ac.nz>
2018-03-22 10:40:08 +1300

    Refractored modify field actions to use a generic action class - Created ModifyObjectByFieldAction - Moved logic from the ModifyXAction to the new object action - Throws the expected errors on construction #refractor #story[433]

--- src/main/java/seng302/Actions/ModifyClinicianAction.java
+++ src/main/java/seng302/Actions/ModifyClinicianAction.java
@@ -1,22 +1,16 @@
 package seng302.Actions;
 
 import seng302.Clinician;
-import seng302.Donor;
-import seng302.Utilities.PrimitiveConverter;
 
-import java.beans.Statement;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * A reversible clinician modification Action
  */
 public class ModifyClinicianAction implements Action {
-    private Map<String, Object> executors = new HashMap<>();
-    private Map<String, Object> unExecutors = new HashMap<>();
+
+    private ArrayList<ModifyObjectByFieldAction> actions = new ArrayList<>();
     private Clinician clinician;
-    private Method[] clinicianMethods;
 
     /**
      * Create a new Action
@@ -24,57 +18,30 @@ public class ModifyClinicianAction implements Action {
      */
     public ModifyClinicianAction(Clinician clinician) {
         this.clinician = clinician;
-        clinicianMethods = clinician.getClass().getMethods();
     }
 
     /**
      * Add a modification to the clinician
-     * @param field The setter field of the clinician. Must match a valid setter in the Clinician object
+     *
+     * @param field    The setter field of the clinician. Must match a valid setter in the Clinician object
      * @param oldValue The object the field initially had. Should be taken from the Clinicians equivalent getter
      * @param newValue The object the field should be update to. Must match the setters Object type
      */
     public void addChange(String field, Object oldValue, Object newValue) throws NoSuchMethodException, NoSuchFieldException {
-        for (Method m : clinicianMethods) {
-            if (m.getName().equals(field)) {
-                if (m.getParameterCount() != 1) {
-                    throw new NoSuchFieldException("Method expects more than one field");
-                }
-                PrimitiveConverter converter = new PrimitiveConverter();
-                Class<?> expectedClass = converter.convertToWrapper(m.getParameterTypes()[0]);
-                if ((newValue != null && newValue.getClass() != expectedClass) || (oldValue != null && oldValue.getClass() != expectedClass)) {
-                    throw new NoSuchFieldException("Method expects a different field type than the one given");
-                }
-                executors.put(field, newValue);
-                unExecutors.put(field, oldValue);
-                return;
-            }
-        }
-        throw new NoSuchMethodException("Donor does not contain that method");
+        actions.add(new ModifyObjectByFieldAction(clinician, field, oldValue, newValue));
     }
 
     @Override
     public void execute() {
-        runChanges(executors);
+        for (ModifyObjectByFieldAction action : actions) {
+            action.execute();
+        }
     }
 
     @Override
     public void unExecute() {
-        runChanges(unExecutors);
-    }
-
-    /**
-     * Loops through a map of String, Objects and applies the Java.beans.Statement to it using the String as the setter and the Object as the parameter
-     * @param map A map of String, Objects
-     */
-    private void runChanges(Map<String, Object> map) {
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            try {
-                Object[] var = {entry.getValue()};
-                Statement s = new Statement(clinician, entry.getKey(), var);
-                s.execute();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        for (ModifyObjectByFieldAction action : actions) {
+            action.unExecute();
         }
     }
 }
--- src/main/java/seng302/Actions/ModifyDonorAction.java
+++ src/main/java/seng302/Actions/ModifyDonorAction.java
@@ -1,22 +1,15 @@
 package seng302.Actions;
 
 import seng302.Donor;
-import seng302.Utilities.PrimitiveConverter;
-
-import java.beans.Statement;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * A reversible donor modification Action
  */
 public class ModifyDonorAction implements Action {
 
-    private Map<String, Object> executors = new HashMap<>();
-    private Map<String, Object> unExecutors = new HashMap<>();
+    private ArrayList<ModifyObjectByFieldAction> actions = new ArrayList<>();
     private Donor donor;
-    private Method[] donorMethods;
 
     /**
      * Create a new Action
@@ -24,7 +17,6 @@ public class ModifyDonorAction implements Action {
      */
     public ModifyDonorAction(Donor donor) {
         this.donor = donor;
-        donorMethods = donor.getClass().getMethods();
     }
 
     /**
@@ -34,47 +26,20 @@ public class ModifyDonorAction implements Action {
      * @param newValue The object the field should be update to. Must match the setters Object type
      */
     public void addChange(String field, Object oldValue, Object newValue) throws NoSuchMethodException, NoSuchFieldException {
-        for (Method m : donorMethods) {
-            if (m.getName().equals(field)) {
-                if (m.getParameterCount() != 1) {
-                    throw new NoSuchFieldException("Method expects more than one field");
-                }
-                PrimitiveConverter converter = new PrimitiveConverter();
-                Class<?> expectedClass = converter.convertToWrapper(m.getParameterTypes()[0]);
-                if ((newValue != null && newValue.getClass() != expectedClass) || (oldValue != null && oldValue.getClass() != expectedClass)) {
-                    throw new NoSuchFieldException("Method expects a different field type than the one given");
-                }
-                executors.put(field, newValue);
-                unExecutors.put(field, oldValue);
-                return;
-            }
-        }
-        throw new NoSuchMethodException("Donor does not contain that method");
+        actions.add(new ModifyObjectByFieldAction(donor, field, oldValue, newValue));
     }
 
     @Override
     public void execute() {
-        runChanges(executors);
+        for (ModifyObjectByFieldAction action : actions) {
+            action.execute();
+        }
     }
 
     @Override
     public void unExecute() {
-        runChanges(unExecutors);
-    }
-
-    /**
-     * Loops through a map of String, Objects and applies the Java.beans.Statement to it using the String as the setter and the Object as the parameter
-     * @param map A map of String, Objects
-     */
-    private void runChanges(Map<String, Object> map) {
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            try {
-                Object[] var = {entry.getValue()};
-                Statement s = new Statement(donor, entry.getKey(), var);
-                s.execute();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        for (ModifyObjectByFieldAction action : actions) {
+            action.unExecute();
         }
     }
 }
