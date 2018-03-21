6adfb5f1f4ef58776725e977d0dd6f4e6e278287
alextompkins <ato47@uclive.ac.nz>
2018-03-22 10:16:29 +1300

    Added a new clinician sidebar -Implemented on search donors page. #implement #story[503]
    Took 25 minutes

--- src/main/java/seng302/Utilities/Page.java
+++ src/main/java/seng302/Utilities/Page.java
@@ -6,6 +6,7 @@ public enum Page {
 	LOGIN("/fxml/login.fxml"),
 	CREATE_USER("/fxml/create_donor.fxml"),
 	SIDEBAR("/fxml/sidebar.fxml"),
+	CLINICIAN_SIDEBAR("/fxml/clinician_sidebar.fxml"),
 	VIEW_DONOR("/fxml/view_donor.fxml"),
 	REGISTER_ORGANS("/fxml/register_organs.fxml"),
 	HISTORY("/fxml/history.fxml"),
