536821070381e9355cb69e60c89fd58f9a823493
Alex Tompkins <ato47@uclive.ac.nz>
2018-03-30 01:52:21 +1300

    Implemented usage of the google http client library for requests to the MAPI drug autocompletion API -Created new Utilities/Web package -Created new HTTPUtils class for utility methods that require the HTTP client -Created new MedAutoCompleteURL and MedAutoCompleteResponse classes to implement interactions with the MAPI drug autocompletion API. #implement #story[541]

---
+++ src/main/java/seng302/Utilities/Web/HTTPUtils.java
@@ -0,0 +1,49 @@
+package seng302.Utilities.Web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.gson.GsonFactory;
+
+public class HTTPUtils {
+
+    private static final String AUTOCOMPLETE_ENDPOINT = "http://mapi-us.iterar.co/api/autocomplete";
+
+    private static HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static JsonFactory JSON_FACTORY = new GsonFactory();
+
+    public static List<String> getMedAutoCompleteSuggestions(String queryString) {
+        List<String> suggestions = new ArrayList<>();
+
+        HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(
+                (HttpRequest request) -> {
+                    request.setParser(new JsonObjectParser(JSON_FACTORY));
+                });
+
+        try {
+            MedAutoCompleteURL url = new MedAutoCompleteURL(AUTOCOMPLETE_ENDPOINT);
+            url.setQueryString(queryString);
+            HttpRequest request = requestFactory.buildGetRequest(url);
+            suggestions = request.execute().parseAs(MedAutoCompleteResponse.class).getSuggestions();
+        } catch (HttpResponseException exc) {
+            // TODO handle more gracefully
+            exc.printStackTrace();
+        } catch (IOException exc) {
+            // TODO handle more gracefully
+            exc.printStackTrace();
+        }
+        return suggestions;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMedAutoCompleteSuggestions("pan"));
+    }
+}
