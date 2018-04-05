68153bba388259a4c85ab03f56a4e80da2248d94
Alex Tompkins <ato47@uclive.ac.nz>
2018-04-05 19:43:58 +1200

    Javadoc-d ViewMedicationsController. #document #story[537]
    Took 22 minutes

--- src/main/java/seng302/Controller/Donor/ViewMedicationsController.java
+++ src/main/java/seng302/Controller/Donor/ViewMedicationsController.java
@@ -25,6 +25,9 @@ import seng302.State.State;
 
 import impl.org.controlsfx.autocompletion.AutoCompletionTextFieldBinding;
 
+/**
+ * Controller for the view/edit medications page.
+ */
 public class ViewMedicationsController extends SubController {
 
     private Session session;
@@ -51,6 +54,12 @@ public class ViewMedicationsController extends SubController {
         session = State.getSession();
     }
 
+    /**
+     * Initializes the UI for this page.
+     * - Sets listeners for changing selection on two list views so that if an item is selected on one, the selection
+     * is removed from the other.
+     * - Checks if the logged in user is a donor, and if so, makes the page non-editable.
+     */
     @FXML
     private void initialize() {
         new AutoCompletionTextFieldBinding<>(newMedField, param -> getSuggestions(newMedField.getText()));
@@ -77,6 +86,13 @@ public class ViewMedicationsController extends SubController {
         }
     }
 
+    /**
+     * Sets up the page using the MainController given.
+     * - Loads the sidebar.
+     * - Checks if the session login type is a donor or a clinician, and sets the viewed donor appropriately.
+     * - Refreshes the medication list views to set initial state based on the viewed donor.
+     * @param mainController The MainController for the window this page is loaded on.
+     */
     @Override
     public void setup(MainController mainController) {
         super.setup(mainController);
@@ -91,13 +107,23 @@ public class ViewMedicationsController extends SubController {
         refreshMedicationLists();
     }
 
+    /**
+     * Refreshes the past/current medication list views from the donor's properties.
+     */
     private void refreshMedicationLists() {
         pastMedicationsView.setItems(FXCollections.observableArrayList(donor.getPastMedications()));
         currentMedicationsView.setItems(FXCollections.observableArrayList(donor.getCurrentMedications()));
     }
 
+    /**
+     * Moves the MedicationHistoryItem selected in the current medications list to the past medications list. Also:
+     * - Sets the date the donor stopped taking the medication to the current date.
+     * - Removes the MedicationHistoryItem from the current medications list.
+     * - Refreshes both list views.
+     * @param event When the '<' button is pressed.
+     */
     @FXML
-    private void moveMedicationToHistory(ActionEvent actionEvent) {
+    private void moveMedicationToHistory(ActionEvent event) {
         MedicationHistoryItem item = currentMedicationsView.getSelectionModel().getSelectedItem();
         if (item != null) {
             item.setStopped(LocalDate.now());
@@ -107,8 +133,16 @@ public class ViewMedicationsController extends SubController {
         }
     }
 
+    /**
+     * Moves the MedicationHistoryItem selected in the past medications list to the current medications list. Also:
+     * - Sets the date the donor started taking the medication to the current date.
+     * - Sets the date the donor stopped taking the medication to null (hasn't stopped yet).
+     * - Removes the MedicationHistoryItem from the past medications list.
+     * - Refreshes both list views.
+     * @param event When the '>' button is pressed.
+     */
     @FXML
-    private void moveMedicationToCurrent(ActionEvent actionEvent) {
+    private void moveMedicationToCurrent(ActionEvent event) {
         MedicationHistoryItem item = pastMedicationsView.getSelectionModel().getSelectedItem();
         if (item != null) {
             item.setStarted(LocalDate.now());
@@ -119,6 +153,11 @@ public class ViewMedicationsController extends SubController {
         }
     }
 
+    /**
+     * Checks whether the key pressed was ENTER, and if so, adds a new medication with the current value of the new
+     * medication text field.
+     * @param keyEvent When a key is pressed and focus is on the new medication text field.
+     */
     @FXML
     public void newMedKeyPressed(KeyEvent keyEvent) {
         if (keyEvent.getCode().equals(KeyCode.ENTER)) {
@@ -126,17 +165,32 @@ public class ViewMedicationsController extends SubController {
         }
     }
 
+    /**
+     * Adds a new medication with the current value of the new medication text field.
+     * @param event When the 'add medication' button is pressed.
+     */
     @FXML
     private void addButtonPressed(ActionEvent event) {
         addMedication(newMedField.getText());
     }
 
+    /**
+     * Creates a new MedicationHistoryItem for a medication with the given name, sets its 'started' date to the
+     * current date, then adds it to the donor's current medications list.
+     * @param newMedName The name of the medication to add a new instance of.
+     */
     private void addMedication(String newMedName) {
         donor.getCurrentMedications().add(new MedicationHistoryItem(newMedName, LocalDate.now(), null));
         newMedField.setText("");
         refreshMedicationLists();
     }
 
+    /**
+     * Deletes the currently selected MedicationHistoryItem. Will determine which of the list views is currently
+     * selected, then delete from the appropriate one. If neither list view is currently selected, this will have no
+     * effect.
+     * @param event When the 'delete' button is clicked.
+     */
     @FXML
     private void deleteMedication(ActionEvent event) {
         MedicationHistoryItem item = selectedListView.getSelectionModel().getSelectedItem();
