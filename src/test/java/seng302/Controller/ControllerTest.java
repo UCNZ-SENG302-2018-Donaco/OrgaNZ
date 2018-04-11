446ffce94d98ddef07ae41ba44dfe74aad927008
Alex Tompkins <ato47@uclive.ac.nz>
2018-04-11 22:54:55 +1200

    Implemented unit testing of the ViewMedications controller using the TestFX library. -Added testfx-core and testfx-junit dependencies to pom.xml -Created a generic ControllerTest abstract class that can be extended to easily write TestFX tests for any controller, allowing the setup of initial conditions as desired. -Created ViewMedicationsControllerDonorTest and ViewMedicationsControllerClinicianTest, which contain TestFX tests for the donor and clinician versions of the page respectively. -Modified ViewMedicationsController slightly to move all logic that checks for the current session's user type into the setup() method. #test #story[537]
    Took 5 hours 34 minutes

---
+++ src/test/java/seng302/Controller/ControllerTest.java
@@ -0,0 +1,45 @@
+package seng302.Controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import seng302.Utilities.View.Page;
+
+import org.testfx.framework.junit.ApplicationTest;
+
+public abstract class ControllerTest extends ApplicationTest {
+    protected MainController mainController;
+    protected SubController pageController;
+    protected Node pageNode;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Load main pane and controller
+        FXMLLoader mainLoader = new FXMLLoader(getClass().getResource(Page.MAIN.getPath()));
+        Pane mainPane = mainLoader.load();
+        mainController = mainLoader.getController();
+
+        initState();
+
+        // Load page's node and controller
+        FXMLLoader pageLoader = new FXMLLoader(getClass().getResource(getPage().getPath()));
+        pageNode = pageLoader.load();
+        pageController = pageLoader.getController();
+
+        // Setup main controller and set stage's scene to its main pane, then show stage
+        mainController.setStage(stage);
+        stage.setScene(new Scene(mainPane));
+        stage.show();
+
+        // Setup both main and page controller
+        pageController.setup(mainController);
+        mainController.setPage(getPage(), pageNode);
+    }
+
+    protected abstract Page getPage();
+
+    protected abstract void initState();
+}
