8a401cb984519cb27d59499dbfdae672973ab53b
Dylan <dca87@uclive.ac.nz>
2018-04-22 16:55:49 +1200

    Added in history log for adding in a medication TODO: Add in the rest of history items such as: - delete medication - compare medication - moving the medication from either current or history lists
    #implement
    #story543

--- src/main/java/seng302/Actions/Donor/AddMedicationRecordAction.java
+++ src/main/java/seng302/Actions/Donor/AddMedicationRecordAction.java
@@ -2,7 +2,9 @@ package seng302.Actions.Donor;
 
 import seng302.Actions.Action;
 import seng302.Donor;
+import seng302.HistoryItem;
 import seng302.MedicationRecord;
+import seng302.Utilities.JSONConverter;
 
 public class AddMedicationRecordAction implements Action {
 
@@ -12,6 +14,9 @@ public class AddMedicationRecordAction implements Action {
     public AddMedicationRecordAction(Donor donor, MedicationRecord record) {
         this.donor = donor;
         this.record = record;
+        HistoryItem save = new HistoryItem("ADD_MEDICATION", String.format("Medication %s added for %s %s",
+                record.getMedicationName(), donor.getFirstName(), donor.getLastName()));
+        JSONConverter.updateHistory(save, "action_history.json");
     }
 
     @Override
