a49844121fdd5e0e120cd49a94c716ffbef02a05
Alex Tompkins <ato47@uclive.ac.nz>
2018-04-26 12:47:24 +1200

    Added missing override annotation.
    Took 4 minutes

--- src/main/java/seng302/Controller/Clinician/SearchDonorsController.java
+++ src/main/java/seng302/Controller/Clinician/SearchDonorsController.java
@@ -148,6 +148,7 @@ public class SearchDonorsController extends SubController {
      * Upon filtering update, refresh the filters to the new string and update pagination
      * Every refresh triggers the pagination to update and go to page zero
      */
+    @Override
     public void refresh() {
         String searchText = searchBox.getText();
         if (searchText == null || searchText.length() == 0) {
