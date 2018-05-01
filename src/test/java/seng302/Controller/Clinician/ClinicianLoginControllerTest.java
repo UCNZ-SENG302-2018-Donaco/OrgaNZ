7d0314022871941d33e966ffc0a799647dde0b23
jto59 <jto59@uclive.ac.nz>
2018-05-01 12:53:41 +1200

    ClinicianLoginController tests #test story[622]

--- src/test/java/seng302/Controller/Clinician/ClinicianLoginControllerTest.java
+++ src/test/java/seng302/Controller/Clinician/ClinicianLoginControllerTest.java
@@ -12,22 +12,52 @@ import seng302.Utilities.View.WindowContext;
 import static org.junit.Assert.*;
 
 public class ClinicianLoginControllerTest extends ControllerTest {
-    private Clinician testClinician = new Clinician("Mr", null, "Tester", "9 Fake St", Region.AUCKLAND, 1000, "qwerty");
+    private Clinician testClinician = new Clinician("Mr", null, "Tester", "9 Fake St", Region.AUCKLAND, 3, "k");
 
     @Override
     protected Page getPage() {
-        return Page.CREATE_CLINICIAN;
+        return Page.LOGIN_CLINICIAN;
     }
 
     @Override
     protected void initState() {
         State.init();
+        State.getClinicianManager().addClinician(testClinician);
         mainController.setWindowContext(WindowContext.defaultContext());
     }
 
     @Test
     public void loginDefaultAdmin() {
+        clickOn("#staffId").write("0");
+        clickOn("#password").write("admin");
+        clickOn("#signInButton");
+        assertEquals(Page.VIEW_CLINICIAN, mainController.getCurrentPage());
+    }
 
+    @Test
+    public void loginTestAdmin() {
+        clickOn("#staffId").write("3");
+        clickOn("#password").write("k");
+        clickOn("#signInButton");
+        assertEquals(Page.VIEW_CLINICIAN, mainController.getCurrentPage());
+    }
+
+    @Test
+    public void nonExistingId() {
+        clickOn("#staffId").write("9");
+        clickOn("#password").write("k");
+        clickOn("#signInButton");
+        clickOn("OK");
+        assertEquals(Page.LOGIN_CLINICIAN, mainController.getCurrentPage());
+    }
+
+    @Test
+    public void incorrectPassword() {
+        clickOn("#staffId").write("0");
+        clickOn("#password").write("k");
+        clickOn("#signInButton");
+        clickOn("OK");
+        assertEquals(Page.LOGIN_CLINICIAN, mainController.getCurrentPage());
     }
 
 }
