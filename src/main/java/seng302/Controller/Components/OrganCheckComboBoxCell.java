f8692dde4a2bdba281b34e80e57df7111c16fa0e
Alex Tompkins <ato47@uclive.ac.nz>
2018-05-03 03:41:54 +1200

    Javadoc-d cell components. #document #story[620]
    Took 13 minutes

--- src/main/java/seng302/Controller/Components/OrganCheckComboBoxCell.java
+++ src/main/java/seng302/Controller/Components/OrganCheckComboBoxCell.java
@@ -14,9 +14,19 @@ import seng302.Utilities.Enums.Organ;
 
 import org.controlsfx.control.CheckComboBox;
 
+/**
+ * An editable table cell that holds a {@link CheckComboBox} for organ values. Changing the checked items triggers an
+ * edit commit on the cell.
+ * @param <T> The type of data record each row in the table represents.
+ */
 public class OrganCheckComboBoxCell<T> extends TableCell<T, Set<Organ>> {
     private final CheckComboBox<Organ> checkComboBox;
 
+    /**
+     * Creates a new organ {@link CheckComboBox} cell for the given column. Also binds the disabled property to that of
+     * the table.
+     * @param column The {@link Set<Organ>} column to create a date picker cell for.
+     */
     public OrganCheckComboBoxCell(TableColumn<T, Set<Organ>> column) {
         checkComboBox = new CheckComboBox<>();
         checkComboBox.getItems().setAll(Organ.values());
@@ -36,6 +46,12 @@ public class OrganCheckComboBoxCell<T> extends TableCell<T, Set<Organ>> {
         setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
     }
 
+    /**
+     * Triggered whenever the {@link Set<Organ>} value of the cell is updated; it sets that new value in the
+     * {@link CheckComboBox}.
+     * @param item The new set of organs.
+     * @param empty Whether the cell is now empty or not.
+     */
     @Override
     protected void updateItem(Set<Organ> item, boolean empty) {
         super.updateItem(item, empty);
