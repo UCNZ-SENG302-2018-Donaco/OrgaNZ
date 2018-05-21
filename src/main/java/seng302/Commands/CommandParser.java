b5e9d89125f64acc4c5e2419bb7cbf9335185863
Jack Steel <jes143@uclive.ac.nz>
2018-05-21 17:30:02 +1200

    Updated CommandParser to allow backslash escaped double quotes and to handle equals with quotes properly #fix #story[709]

--- src/main/java/seng302/Commands/CommandParser.java
+++ src/main/java/seng302/Commands/CommandParser.java
@@ -9,17 +9,44 @@ public class CommandParser {
 
     /**
      * Takes a string line in the form of a command, and returns the space separated items. Double
-     * quoted strings are considered one item
+     * quoted strings are considered one item. Also allows double quotes to be escaped using a backslash
      * @param input The string to parse
      * @return A string list of space separated items
      */
     public static String[] parseCommands(String input) {
         List<String> inputs = new ArrayList<>();
-        //Regex matcher that separates on space but allows for double quoted strings to be considered single strings
-        Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(input);
-        while (m.find()) {
-            inputs.add(m.group(1).replace("\"", ""));
+
+        String currentItem = "";
+        boolean betweenQuotes = false;
+        boolean lastCharWasBackSlash = false;
+
+        for (char ch : input.toCharArray()) {
+            if ((ch == ' ' || ch == '=') && !betweenQuotes) {
+                if (currentItem.equals("")) {
+                    continue;
+                }
+                inputs.add(currentItem);
+                currentItem = "";
+            } else if (ch == '"') {
+                if (lastCharWasBackSlash) {
+                    currentItem = currentItem.substring(0, currentItem.length() -1);
+                    currentItem += ch;
+                    continue;
+                }
+                if (betweenQuotes) {
+                    betweenQuotes = false;
+                    inputs.add(currentItem);
+                    currentItem = "";
+                } else {
+                    betweenQuotes = true;
+                }
+            } else {
+                currentItem += ch;
+            }
+            lastCharWasBackSlash = ch == '\\';
         }
+        inputs.add(currentItem);
+
         return inputs.toArray(new String[0]);
     }
 
