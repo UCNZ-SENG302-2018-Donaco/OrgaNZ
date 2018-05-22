27620d8dc97cccb694c54b08e60414504d94b51f
Jack Steel <jes143@uclive.ac.nz>
2018-05-23 00:16:29 +1200

    Updated the min width and height to: 639x1016. These are the exact values we initially set the window to, as any downsizing currently causes issues. #fix
    Took 5 minutes

--- src/main/java/seng302/AppUI.java
+++ src/main/java/seng302/AppUI.java
@@ -43,6 +43,8 @@ public class AppUI extends Application {
         LoggerSetup.setup(Level.INFO);
 
         stage.setTitle("Organ Client Management System");
+        stage.setMinHeight(639);
+        stage.setMinWidth(1016);
         stage.setScene(createScene(loadMainPane(stage)));
         stage.show();
 
