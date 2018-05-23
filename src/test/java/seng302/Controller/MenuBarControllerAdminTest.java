5f52e35a1178a4540322265cdcdbc180c3908b08
Dylan Carlyle <dca87@uclive.ac.nz>
2018-05-24 11:57:44 +1200

    Added four new Test classes for the Menu bar. They are to test: - An Admin default view (when logging in) - A Clinician default view (when logging in) - An Admin view when viewing a client - A Clinician view when viewing a client #implement #story[773]

---
+++ src/test/java/seng302/Controller/MenuBarControllerAdminTest.java
@@ -0,0 +1,17 @@
+package seng302.Controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MenuBarControllerAdminTest {
+
+    @Test
+    public void setup() {
+    }
+
+    @Test
+    public void refresh() {
+    }
+
+}
