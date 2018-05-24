4a92dbb66b409fb809c283afca8d61e0259e3ebd
Ollie <och26@uclive.ac.nz>
2018-05-24 15:09:05 +1200

    Fix merge

--- src/main/java/seng302/AppUI.java
+++ src/main/java/seng302/AppUI.java
@@ -46,8 +46,8 @@ public class AppUI extends Application {
         primaryStage.setScene(createScene(loadMainPane(primaryStage)));
         primaryStage.show();
 
-        stage.setMinHeight(639);
-        stage.setMinWidth(1016);
+        primaryStage.setMinHeight(639);
+        primaryStage.setMinWidth(1016);
 
         State.init();
 
