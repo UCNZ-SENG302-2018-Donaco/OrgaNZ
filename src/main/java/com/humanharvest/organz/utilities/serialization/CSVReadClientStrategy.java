6b32c28c0ab82017fc1b26510a40583d078ebb0b
Alex Tompkins <ato47@uclive.ac.nz>
2018-06-20 15:53:55 +1200

    Improve CSVReadClientStrategy, remove support for multiple date formats as no longer required (sample data was fixed). #implement #story[781] !task[b]
    Took 14 minutes

--- src/main/java/com/humanharvest/organz/utilities/serialization/CSVReadClientStrategy.java
+++ src/main/java/com/humanharvest/organz/utilities/serialization/CSVReadClientStrategy.java
@@ -14,16 +14,18 @@ import org.apache.commons.csv.CSVRecord;
 
 public class CSVReadClientStrategy {
 
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
     public enum Header {
-        CODE, FIRST, LAST, DATE_OF_BIRTH, DATE_OF_DEATH, BIRTH_GENDER, GENDER_IDENTITY, BLOOD_TYPE, HEIGHT, WEIGHT,
-        ADDRESS_NUMBER, ADDRESS_STREET, ADDRESS_SUBURB, ADDRESS_CITY, REGION, POSTCODE, BIRTH_COUNTRY_CODE,
-        CURR_COUNTRY_CODE, PHONE_HOME, PHONE_MOBILE, EMAIL
+        NHI, FIRST_NAME, LAST_NAME, DATE_OF_BIRTH, DATE_OF_DEATH, BIRTH_GENDER, GENDER_IDENTITY, BLOOD_TYPE, HEIGHT,
+        WEIGHT, STREET_NUMBER, STREET_NAME, NEIGHBORHOOD, CITY, REGION, POSTCODE, CURRENT_COUNTRY, BIRTH_COUNTRY,
+        PHONE_HOME, PHONE_MOBILE, EMAIL
     }
 
-    public Client parse(CSVRecord record) throws IllegalArgumentException {
+    public Client deserialise(CSVRecord record) throws IllegalArgumentException {
         Client client = new Client();
-        client.setFirstName(record.get(FIRST));
-        client.setLastName(record.get(LAST));
+        client.setFirstName(record.get(FIRST_NAME));
+        client.setLastName(record.get(LAST_NAME));
         client.setDateOfBirth(parseDate(record.get(DATE_OF_BIRTH)));
         client.setDateOfDeath(parseDate(record.get(DATE_OF_DEATH)));
         client.setGender(Gender.fromString(record.get(BIRTH_GENDER)));
@@ -32,27 +34,20 @@ public class CSVReadClientStrategy {
         client.setHeight(Double.parseDouble(record.get(HEIGHT)));
         client.setWeight(Double.parseDouble(record.get(WEIGHT)));
         client.setCurrentAddress(
-                record.get(ADDRESS_NUMBER) + " " +
-                record.get(ADDRESS_STREET) + ", " +
-                record.get(ADDRESS_SUBURB) + ", " +
-                record.get(ADDRESS_CITY) + ", " +
+                record.get(STREET_NUMBER) + " " +
+                record.get(STREET_NAME) + ", " +
+                record.get(NEIGHBORHOOD) + ", " +
+                record.get(CITY) + ", " +
                 record.get(POSTCODE));
         client.setRegion(Region.fromString(record.get(REGION)));
         return client;
     }
 
     private LocalDate parseDate(String string) throws IllegalArgumentException {
-        final DateTimeFormatter slashFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        final DateTimeFormatter dashFormat = DateTimeFormatter.ofPattern("dd-MM-yy");
-
         try {
-            return LocalDate.parse(string, slashFormat);
-        } catch (DateTimeParseException exc1) {
-            try {
-                return LocalDate.parse(string, dashFormat);
-            } catch (DateTimeParseException exc2) {
-                throw new IllegalArgumentException("Given text did not match either accepted date format.");
-            }
+            return LocalDate.parse(string, dateFormat);
+        } catch (DateTimeParseException exc) {
+            throw new IllegalArgumentException(exc);
         }
     }
 }
