c6a86e338da43ae9ae9787b1baec4f496eb548b8
Thomas Kearsley <tke29@uclive.ac.nz>
2018-07-12 13:32:53 +1200

    Added GET Mappings for current Illnesses and Past Illnesses

--- server/src/main/java/com/humanharvest/organz/server/controller/client/ClientIllnessessController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/client/ClientIllnessessController.java
@@ -1,8 +1,61 @@
 package com.humanharvest.organz.server.controller.client;
 
+import com.humanharvest.organz.Client;
+import com.humanharvest.organz.IllnessRecord;
+import com.humanharvest.organz.state.State;
+import com.sun.org.apache.regexp.internal.RE;
+import java.util.List;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientIllnessessController {
+
+  /**
+   * Gets Clients Current Illnesses
+   * @param id Id Of Client
+   * @return Returns list of Current Illnesses
+   */
+  @GetMapping("/clients/{id}/current-illnesses")
+  public ResponseEntity<List<IllnessRecord>> getClientCurrentIllnesses(@PathVariable int id) {
+      Client client = State.getClientManager().getClientByID(id);
+      // Client does not exist
+      if (client == null){
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      } else {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("ETag", client.getEtag());
+
+        return new ResponseEntity<List<IllnessRecord>>(client.getCurrentIllnesses(),headers,HttpStatus.OK);
+      }
+
+  }
+
+  /**
+   * Gets Clients Past Illnesses
+   * @param id Id Of Client
+   * @return Returns list of past Illnesses, if client exists.
+   */
+  @GetMapping("/clients/{id}/past-illnesses")
+  public ResponseEntity<List<IllnessRecord>> getClientPastIllnesses(@PathVariable int id){
+    Client client = State.getClientManager().getClientByID(id);
+    if (client == null){
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    } else {
+      HttpHeaders headers = new HttpHeaders();
+      headers.add("ETag",client.getEtag());
+      return  new ResponseEntity<List<IllnessRecord>>(client.getPastIllnesses(),headers,HttpStatus.OK);
+    }
+
+
+  }
+
 
 }
--- server/src/main/java/com/humanharvest/organz/server/controller/client/ClientMedicationsController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/client/ClientMedicationsController.java
@@ -5,4 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientMedicationsController {
 
+
 }
