3da8f762115a8ec99dc99b30eed8a3b344bf37b0
Ollie <och26@uclive.ac.nz>
2018-07-18 13:18:13 +1200

    Add server implementation of POST /clients/{id}/transplantRequests #story[782]

--- server/src/main/java/com/humanharvest/organz/server/controller/client/ClientTransplantRequestsController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/client/ClientTransplantRequestsController.java
@@ -3,13 +3,17 @@ package com.humanharvest.organz.server.controller.client;
 import java.util.Collection;
 import java.util.Optional;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.humanharvest.organz.Client;
 import com.humanharvest.organz.TransplantRequest;
 import com.humanharvest.organz.state.State;
+import com.humanharvest.organz.views.client.Views;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +27,31 @@ public class ClientTransplantRequestsController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    /**
+     * Creates a transplant request.
+     * @param id the client's ID
+     * @return list of all transplant requests for that client
+     */
+    @PostMapping("/clients/{id}/transplantRequests")
+    @JsonView(Views.Overview.class)
+    public ResponseEntity<Collection<TransplantRequest>> postTransplantRequest(
+            @RequestBody TransplantRequest transplantRequest,
+            @PathVariable int id) {
+        Optional<Client> client = State.getClientManager().getClientByID(id);
+
+        if (client.isPresent()) {
+            client.get().addTransplantRequest(transplantRequest);
+
+            Collection<TransplantRequest> transplantRequests = client.get().getTransplantRequests();
+            return new ResponseEntity<>(transplantRequests, HttpStatus.OK);
+
+        } else {
+            // no client exists with that ID
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+
     }
 }
