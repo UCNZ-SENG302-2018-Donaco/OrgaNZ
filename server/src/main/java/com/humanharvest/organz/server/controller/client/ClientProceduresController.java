5ad6638d9dd28e85635d5f51f656160330969a90
alextompkins <ato47@uclive.ac.nz>
2018-07-19 13:18:29 +1200

    Implement scaffold for all procedures endpoints #implement #story[782] !task[pa]
    Took 36 minutes

--- server/src/main/java/com/humanharvest/organz/server/controller/client/ClientProceduresController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/client/ClientProceduresController.java
@@ -1,8 +1,42 @@
 package com.humanharvest.organz.server.controller.client;
 
+import java.util.Collection;
+
+import com.humanharvest.organz.ProcedureRecord;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Provides handlers for requests to these endpoints:
+ * - GET /clients/{uid}/procedures
+ * - POST /clients/{uid}/procedures
+ * - PATCH /clients/{uid}/procedures/{id}
+ * - DELETE /clients/{uid}/procedures/{id}
+ */
 @RestController
 public class ClientProceduresController {
 
+    @GetMapping("/clients/{uid}/procedures")
+    public ResponseEntity<Collection<ProcedureRecord>> getProceduresForClient() {
+        throw new UnsupportedOperationException();
+    }
+
+    @PostMapping("/clients/{uid}/procedures")
+    public ResponseEntity<Collection<ProcedureRecord>> createProcedureRecord() {
+        throw new UnsupportedOperationException();
+    }
+
+    @PatchMapping("/clients/{uid}/procedures/{id)")
+    public ResponseEntity<ProcedureRecord> modifyProcedureRecord() {
+        throw new UnsupportedOperationException();
+    }
+
+    @DeleteMapping("/clients/{uid}/procedures/{id}")
+    public ResponseEntity deleteProcedureRecord() {
+        throw new UnsupportedOperationException();
+    }
 }
