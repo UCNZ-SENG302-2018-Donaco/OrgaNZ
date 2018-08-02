e8ec1a83f4e84adce24f09417ca47beef68fdb49
Matthew Smit <programmdude@gmail.com>
2018-08-02 21:39:27 +1200

    Fake TUIO mouse events.
    #story[1054]

--- client/src/main/java/com/humanharvest/organz/AppTUIO.java
+++ client/src/main/java/com/humanharvest/organz/AppTUIO.java
@@ -1,26 +1,51 @@
 package com.humanharvest.organz;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.event.EventHandler;
+import javafx.event.EventTarget;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
 import TUIO.TuioCursor;
 import com.humanharvest.organz.controller.MainController;
 import com.humanharvest.organz.state.State;
 import com.humanharvest.organz.state.State.DataStorageType;
 import com.humanharvest.organz.utilities.LoggerSetup;
-import com.humanharvest.organz.utilities.view.*;
+import com.humanharvest.organz.utilities.view.Page;
+import com.humanharvest.organz.utilities.view.PageNavigator;
+import com.humanharvest.organz.utilities.view.PageNavigatorTouch;
+import com.humanharvest.organz.utilities.view.TuioFXUtils;
+import com.humanharvest.organz.utilities.view.WindowContext;
 import com.sun.javafx.css.StyleManager;
-import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+import com.sun.javafx.geom.PickRay;
+import com.sun.javafx.scene.input.PickResultChooser;
 import org.tuiofx.Configuration;
 import org.tuiofx.TuioFX;
+import org.tuiofx.internal.base.CoordinatesMapping;
+import org.tuiofx.internal.base.GestureHandler;
+import org.tuiofx.internal.base.TargetSelection;
+import org.tuiofx.internal.base.TouchHandler;
+import org.tuiofx.internal.base.TuioFXCanvas;
 import org.tuiofx.internal.base.TuioInputService;
-
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.logging.Level;
+import org.tuiofx.internal.gesture.TuioTouchPoint;
+import org.tuiofx.internal.util.Util;
 
 /**
  * The main class that runs the JavaFX GUI.
@@ -67,7 +92,7 @@ public class AppTUIO extends Application {
         LoggerSetup.setup(Level.INFO);
         PageNavigator.setPageNavigator(new PageNavigatorTouch());
 
-        final Scene scene = new Scene(root, 1920, 1080);
+        Scene scene = new Scene(root, 1920, 1080);
 
         TuioFX tuioFX = new TuioFX(primaryStage, Configuration.debug());
         //Instead of tuioFX.enableMTWidgets(true);
@@ -136,10 +161,11 @@ public class AppTUIO extends Application {
 
         primaryStage.setTitle("Test");
         primaryStage.setScene(scene);
-        primaryStage.setFullScreen(true);
+//        primaryStage.setFullScreen(true);
         primaryStage.show();
 
-//        initialiseFakeTUIO(tuioFX, primaryStage);
+        initialiseTUIOHook(tuioFX);
+        initialiseFakeTUIO(tuioFX, primaryStage);
 
         State.init(DataStorageType.REST);
 
@@ -148,18 +174,51 @@ public class AppTUIO extends Application {
         }
     }
 
+    private static void initialiseTUIOHook(TuioFX tuioFX) {
+        try {
+            AppTUIO.<TuioInputService>getField(tuioFX, "tuioInputService").disconnectTUIO();
+
+            Configuration config = getField(tuioFX, "config");
+            GestureHandler gestureHandler = new GestureHandler(config);
+            TargetSelection targetSelection = new TargetSelectionHook(getField(tuioFX, "scene"), config);
+            CoordinatesMapping coordinatesMapping = new CoordinatesMapping(getField(tuioFX, "stage"), config);
+            TouchHandler touchHandler = new TouchHandler(gestureHandler, targetSelection, coordinatesMapping, config);
+            TuioInputService inputService = new TuioInputServiceHook(coordinatesMapping, touchHandler, config);
+
+            setField(tuioFX, "gestureHandler", gestureHandler);
+            setField(tuioFX, "targetSelection", targetSelection);
+            setField(tuioFX, "coordinatesMapping", coordinatesMapping);
+            setField(tuioFX, "touchHandler", touchHandler);
+            setField(tuioFX, "tuioInputService", inputService);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static <T> T getField(Object o, String fieldName)
+            throws NoSuchFieldException, IllegalAccessException {
+        Field field = o.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return (T)field.get(o);
+    }
+
+    private static <T> void setField(Object o, String fieldName, T value)
+            throws NoSuchFieldException, IllegalAccessException {
+        Field field = o.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(o, value);
+    }
+
     private static void initialiseFakeTUIO(TuioFX tuioFX, Stage primaryStage) {
-        TuioInputService inputService = null;
+        TuioInputService inputService;
 
         try {
-            Field inputServiceField = tuioFX.getClass().getDeclaredField("tuioInputService");
-            inputServiceField.setAccessible(true);
-            inputService = (TuioInputService)inputServiceField.get(tuioFX);
+            inputService = getField(tuioFX, "tuioInputService");
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
 
-        primaryStage.addEventFilter(MouseEvent.ANY, new TuioMouseEventFilter(inputService));
+        primaryStage.addEventFilter(MouseEvent.ANY, new TuioMouseEventFilter(inputService, primaryStage));
     }
 
     private static double withinRange(double min, double max, double value) {
@@ -169,23 +228,201 @@ public class AppTUIO extends Application {
     private static class TuioMouseEventFilter implements EventHandler<MouseEvent> {
 
         private final TuioInputService inputService;
-        int sessionId = 0;
+        private final Stage primaryStage;
+        private int currentSessionId = 1;
 
-        public TuioMouseEventFilter(TuioInputService inputService) {
+        public TuioMouseEventFilter(TuioInputService inputService, Stage primaryStage) {
             this.inputService = inputService;
+            this.primaryStage = primaryStage;
         }
 
         @Override
         public void handle(MouseEvent event) {
-            if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-                inputService.addTuioCursor(new TuioCursor(0, sessionId++, (float)event.getX(), (float)event.getY()));
-
-            } else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
-                inputService.updateTuioCursor(new TuioCursor(0, sessionId++, (float)event.getX(), (float)event.getY()));
-
-            } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
-                inputService.removeTuioCursor(new TuioCursor(0, sessionId++, (float)event.getX(), (float)event.getY()));
+            if (event.isSynthesized()) {
+                return;
             }
+
+            float x = (float)(event.getSceneX() / primaryStage.getScene().getWidth());
+            float y = (float)(event.getSceneY() / primaryStage.getScene().getHeight());
+            TuioCursor cursor = new TuioCursor(currentSessionId, 0, x, y);
+            if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                inputService.addTuioCursor(cursor);
+            } else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
+                inputService.removeTuioCursor(cursor);
+                ++currentSessionId;
+            } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+                inputService.updateTuioCursor(cursor);
+            }
+        }
+    }
+
+    private static class TuioInputServiceHook extends TuioInputService {
+
+        public TuioInputServiceHook(CoordinatesMapping coordinatesMapping,
+                TouchHandler touchHandler, Configuration config) {
+            super(coordinatesMapping, touchHandler, config);
+        }
+
+        @Override
+        public void addTuioCursor(TuioCursor tuioCursor) {
+            print(tuioCursor);
+            super.addTuioCursor(tuioCursor);
+        }
+
+        @Override
+        public void updateTuioCursor(TuioCursor tuioCursor) {
+            print(tuioCursor);
+            super.updateTuioCursor(tuioCursor);
+        }
+
+        @Override
+        public void removeTuioCursor(TuioCursor tuioCursor) {
+            print(tuioCursor);
+            super.removeTuioCursor(tuioCursor);
+        }
+
+        private void print(TuioCursor tuioCursor) {
+            System.out.printf("SessionID: %d, CursorID: %d, X: %f, Y: %f %n",
+                    tuioCursor.getSessionID(),
+                    tuioCursor.getCursorID(),
+                    tuioCursor.getX(),
+                    tuioCursor.getY());
+        }
+    }
+
+    private static class TargetSelectionHook extends TargetSelection {
+
+        private final Scene scene;
+        private Configuration config;
+
+        public TargetSelectionHook(Scene scene, Configuration config) {
+            super(scene, config);
+            this.scene = scene;
+            this.config = config;
+        }
+
+        @Override
+        public EventTarget selectTargetNode(TuioTouchPoint touchPoint) {
+            double screenX = touchPoint.getScreenX();
+            double screenY = touchPoint.getScreenY();
+            double sceneX = touchPoint.getSceneX();
+            double sceneY = touchPoint.getSceneY();
+            Parent startNode = scene.getRoot();
+            Iterator<Window> windows = Window.impl_getWindows();
+
+            while(windows.hasNext()) {
+                Window window = windows.next();
+                if (scene.getWindow() != window) {
+                    double touchScreenMinX = screenX;
+                    double touchScreenMinY = screenY;
+                    if (config.isUseIndirectInputDevice()) {
+                        touchScreenMinX = screenX + Util.getOffsetX(scene);
+                        touchScreenMinY = screenY + Util.getOffsetY(scene);
+                    }
+
+                    double screenMinX = window.getX() + window.getScene().getX();
+                    double screenMinY = window.getY() + window.getScene().getY();
+                    double screenMaxX = screenMinX + window.getWidth();
+                    double screenMaxY = screenMinY + window.getHeight();
+                    if (Util.isWithIn(touchScreenMinX, screenMinX, screenMaxX)
+                            && Util.isWithIn(touchScreenMinY, screenMinY, screenMaxY)) {
+                        sceneX = touchScreenMinX - Util.getOffsetX(window.getScene());
+                        sceneY = touchScreenMinY - Util.getOffsetY(window.getScene());
+                        startNode = window.getScene().getRoot();
+                        if (!(startNode instanceof TuioFXCanvas)) {
+                            Platform.runLater(() -> addTuioFXCanvas(window.getScene()));
+                        }
+                        break;
+                    }
+                }
+            }
+
+            return hitTest(startNode, startNode, sceneX, sceneY);
+        }
+
+//        private Node hitTest(Parent root, Node node, double x, double y) {
+//            Node targetNode;
+//            Task<PickResultChooser> task = new Task<PickResultChooser>() {
+//                protected PickResultChooser call() {
+//                    PickRay pickRay = new PickRay(x, y, 1.0D, -1.0D / 0.0, 1.0D / 0.0);
+//                    PickResultChooser result = new PickResultChooser();
+//                    root.impl_pickNode(pickRay, result);
+//                    pickRay.getDirectionNoClone().normalize();
+//                    return result;
+//                }
+//            };
+//            Platform.runLater(task);
+//            PickResultChooser result = null;
+//
+//            try {
+//                result = task.get();
+//            } catch (InterruptedException ignored) {
+//            } catch (ExecutionException var12) {
+//                var12.printStackTrace();
+//            }
+//
+//            targetNode = result.getIntersectedNode();
+//            if (targetNode != null) {
+//                targetNode = getPickableNode(targetNode);
+//            }
+//
+//            return targetNode;
+//        }
+
+        private Node hitTest(Parent root, Node node, double x, double y) {
+
+            PickRay pickRay = new PickRay(x, y, 1.0D, -1.0D / 0.0, 1.0D / 0.0);
+            PickResultChooser result = new PickResultChooser();
+            root.impl_pickNode(pickRay, result);
+            pickRay.getDirectionNoClone().normalize();
+
+            Node targetNode = result.getIntersectedNode();
+
+            if (targetNode != null) {
+                targetNode = getPickableNode(targetNode);
+            }
+
+            return targetNode;
+        }
+
+        private Node getPickableNode(Node targetNode) {
+            String pickIfMultiTouch = (String)targetNode.getProperties().get("pickIfMultiTouch");
+            String isTouchTransparent = (String)targetNode.getProperties().get("isTouchTransparent");
+
+            if ((pickIfMultiTouch == null || !pickIfMultiTouch.equals("false")) && (isTouchTransparent == null || !isTouchTransparent.equals("true"))) {
+                return !(targetNode instanceof Text) || !(targetNode.getParent() instanceof Button) && !(targetNode.getParent() instanceof Label) ? targetNode : this.getPickableNode(targetNode.getParent());
+            } else {
+                return getPickableNode(targetNode.getParent());
+            }
+        }
+
+        private void updateTouchPointXY(TuioTouchPoint touchPoint, Scene targetScene) {
+            double sceneX;
+            double sceneY;
+            if (config.isUseIndirectInputDevice()) {
+                sceneX = touchPoint.getScreenX() + scene.getWindow().getX() + scene.getX();
+                sceneY = touchPoint.getScreenY() + scene.getWindow().getY() + scene.getY();
+                touchPoint.setScreenX(sceneX);
+                touchPoint.setScreenY(sceneY);
+            }
+
+            sceneX = touchPoint.getScreenX() - (targetScene.getWindow().getX() + targetScene.getX());
+            sceneY = touchPoint.getScreenY() - (targetScene.getWindow().getY() + targetScene.getY());
+            touchPoint.setSceneX(sceneX);
+            touchPoint.setSceneY(sceneY);
+        }
+
+        private void addTuioFXCanvas(Scene scene) {
+            if (!(scene.getRoot() instanceof TuioFXCanvas)) {
+                TuioFXCanvas tuioFXCanvas = new TuioFXCanvas();
+                Parent oldRoot = scene.getRoot();
+                tuioFXCanvas.getChildren().addAll(oldRoot);
+                scene.setRoot(tuioFXCanvas);
+                oldRoot.setTranslateX(0.0D);
+                oldRoot.setTranslateY(0.0D);
+                oldRoot.getStyleClass().removeAll("root");
+            }
+
         }
     }
 }
