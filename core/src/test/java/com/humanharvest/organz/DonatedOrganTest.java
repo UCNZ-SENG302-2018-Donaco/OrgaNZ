76d46de8f8958236c4c4bfcdbc0e56850fedca3d
Ollie Chick <och26@uclive.ac.nz>
2018-08-04 00:15:04 +1200

    make all tests public in DonatedOrganTest #story[954]

--- core/src/test/java/com/humanharvest/organz/DonatedOrganTest.java
+++ core/src/test/java/com/humanharvest/organz/DonatedOrganTest.java
@@ -3,19 +3,14 @@ package com.humanharvest.organz;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.humanharvest.organz.utilities.enums.Organ;
-import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DonatedOrganTest {
-
-    private static final String FIRST_NAME = "First";
-    private static final String MIDDLE_NAME = "middlename";
-    private static final String PREFERRED_NAME = "Preferred";
 
     private Client donor;
     private Organ organ;
@@ -23,7 +18,7 @@ class DonatedOrganTest {
     private DonatedOrgan donatedOrgan;
 
     @BeforeEach
-    void setup() {
+    public void setup() {
         donor = new Client(1);
         organ = Organ.LUNG; //4-6 hours
         dateTimeOfDonation = LocalDateTime.now();
@@ -32,28 +27,28 @@ class DonatedOrganTest {
     // getDurationUntilExpiry tests
 
     @Test
-    void getDurationUntilExpiryTest() {
+    public void getDurationUntilExpiryTest() {
         dateTimeOfDonation = LocalDateTime.now();
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(Duration.ofHours(6).getSeconds(), donatedOrgan.getDurationUntilExpiry().getSeconds(), 1);
     }
 
     @Test
-    void getDurationUntilExpiryHasntExpiredTest() {
+    public void getDurationUntilExpiryHasntExpiredTest() {
         dateTimeOfDonation = LocalDateTime.now().minusHours(2);
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(Duration.ofHours(4).getSeconds(), donatedOrgan.getDurationUntilExpiry().getSeconds(), 1);
     }
 
     @Test
-    void getDurationUntilExpiryJustExpiredTest() {
+    public void getDurationUntilExpiryJustExpiredTest() {
         dateTimeOfDonation = LocalDateTime.now().minusHours(6);
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(Duration.ZERO, donatedOrgan.getDurationUntilExpiry());
     }
 
     @Test
-    void getDurationUntilExpiryExpiredTest() {
+    public void getDurationUntilExpiryExpiredTest() {
         dateTimeOfDonation = LocalDateTime.now().minusHours(8);
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(Duration.ZERO, donatedOrgan.getDurationUntilExpiry());
@@ -62,34 +57,34 @@ class DonatedOrganTest {
     // getProgressDecimal tests
 
     @Test
-    void getProgressDecimalTest() {
+    public void getProgressDecimalTest() {
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(0, donatedOrgan.getProgressDecimal(), 1e-6);
     }
 
     @Test
-    void getProgressDecimalNotInLowerBoundTest() {
+    public void getProgressDecimalNotInLowerBoundTest() {
         dateTimeOfDonation = LocalDateTime.now().minusHours(3);
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(0.5, donatedOrgan.getProgressDecimal(), 1e-6);
     }
 
     @Test
-    void getProgressDecimalInLowerBoundTest() {
+    public void getProgressDecimalInLowerBoundTest() {
         dateTimeOfDonation = LocalDateTime.now().minusHours(5);
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals((double) 5 / 6, donatedOrgan.getProgressDecimal(), 1e-6);
     }
 
     @Test
-    void getProgressDecimalJustExpiredTest() {
+    public void getProgressDecimalJustExpiredTest() {
         dateTimeOfDonation = LocalDateTime.now().minusHours(6);
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(1, donatedOrgan.getProgressDecimal(), 1e-6);
     }
 
     @Test
-    void getProgressDecimalExpiredTest() {
+    public void getProgressDecimalExpiredTest() {
         dateTimeOfDonation = LocalDateTime.now().minusHours(7);
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(1, donatedOrgan.getProgressDecimal(), 1e-6);
@@ -98,41 +93,41 @@ class DonatedOrganTest {
     // getFullMarker tests
 
     @Test
-    void getFullMarkerLungTest() {
+    public void getFullMarkerLungTest() {
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals((double) 2 / 3, donatedOrgan.getFullMarker());
     }
 
     @Test
-    void getFullMarkerPancreasTest() {
+    public void getFullMarkerPancreasTest() {
         organ = Organ.PANCREAS;
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals((double) 1 / 2, donatedOrgan.getFullMarker());
     }
 
     @Test
-    void getFullMarkerLiverTest() {
+    public void getFullMarkerLiverTest() {
         organ = Organ.LIVER;
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals(1, donatedOrgan.getFullMarker());
     }
 
     @Test
-    void getFullMarkerKidneyTest() {
+    public void getFullMarkerKidneyTest() {
         organ = Organ.KIDNEY;
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals((double) 2 / 3, donatedOrgan.getFullMarker());
     }
 
     @Test
-    void getFullMarkerCorneaTest() {
+    public void getFullMarkerCorneaTest() {
         organ = Organ.CORNEA;
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals((double) 5 / 7, donatedOrgan.getFullMarker());
     }
 
     @Test
-    void getFullMarkerSkinTest() {
+    public void getFullMarkerSkinTest() {
         organ = Organ.SKIN;
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         assertEquals((double) 3 / 10, donatedOrgan.getFullMarker());
@@ -141,7 +136,7 @@ class DonatedOrganTest {
     // todo This should not result in an NPE. Expiration times for organs such as bone marrow are not yet implemented
     @Test
     @Ignore
-    void getFullMarkerBoneMarrowTest() {
+    public void getFullMarkerBoneMarrowTest() {
         organ = Organ.BONE_MARROW;
         donatedOrgan = new DonatedOrgan(organ, donor, dateTimeOfDonation);
         donatedOrgan.getFullMarker();
