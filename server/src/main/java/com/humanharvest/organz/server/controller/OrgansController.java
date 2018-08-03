3e6e22f1e2e7f965ea62d8d839d124d4582b2d9c
Elizabeth Wilson <ewi32@uclive.ac.nz>
2018-08-03 14:43:26 +1200

    Endpoint for getting all organs available for donation implemented
    #document #implement #story[954]

---
+++ server/src/main/java/com/humanharvest/organz/server/controller/OrgansController.java
@@ -0,0 +1,33 @@
+package com.humanharvest.organz.server.controller;
+
+import java.util.Collection;
+
+import com.humanharvest.organz.DonatedOrgan;
+import com.humanharvest.organz.server.exceptions.GlobalControllerExceptionHandler;
+import com.humanharvest.organz.state.State;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrgansController {
+
+    /**
+     * The GET endpoint for getting all organs currently available to be donated
+     * @param authToken authentication token - only clinicians and administrators can access donatable organs
+     * @return response entity containing all organs that are available for donation
+     * @throws GlobalControllerExceptionHandler.InvalidRequestException
+     */
+    @GetMapping("organs")
+    public ResponseEntity<Collection<DonatedOrgan>> getOrgansToDonate(
+            @RequestHeader(value = "X-Auth-Token", required = false) String authToken)
+            throws GlobalControllerExceptionHandler.InvalidRequestException {
+
+        State.getAuthenticationManager().verifyClinicianOrAdmin(authToken);
+
+        Collection<DonatedOrgan> donatedOrgans = State.getClientManager().getAllOrgansToDonate();
+        return new ResponseEntity<>(donatedOrgans, HttpStatus.OK);
+    }
+}
