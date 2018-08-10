256004cc3b6b6284a4049070b9b721a60327ca3e
Ollie Chick <och26@uclive.ac.nz>
2018-08-11 00:04:54 +1200

    Move MatchOrgansToRecipients logic into its own class, in server's utilities.algorithms. Round transplant request times to nearest hour, and start checking location (only some country checks done so far). Add lat and long to Country enum, and load in this data from an xml file (which I added to doc/, as well as a python script I used to turn it into an enum), and add getters. #story[959]

--- server/src/main/java/com/humanharvest/organz/server/controller/clinician/MatchOrgansToRecipientsController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/clinician/MatchOrgansToRecipientsController.java
@@ -13,22 +13,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.humanharvest.organz.utilities.algorithms.MatchOrganToRecipients;
 
 /**
  * Provides handlers for requests to these endpoints:
  * - GET /matchOrganToRecipients
  */
 public class MatchOrgansToRecipientsController {
-
-    public boolean agesMatch(int donorAge, int recipientAge) {
-        // If one is under 12, they must both be under 12
-        if (donorAge < 12 || recipientAge < 12) {
-            return donorAge < 12 && recipientAge < 12;
-        }
-
-        // Otherwise (aged 12+), they must have a max age diff of 15
-        return Math.abs(donorAge - recipientAge) <= 15;
-    }
 
     /**
      * Retrieves a list of all clients who are potential recipients of the organ passed in.
@@ -47,25 +38,7 @@ public class MatchOrgansToRecipientsController {
         // Verify that request has clinician/admin authorization
         State.getAuthenticationManager().verifyClinicianOrAdmin(authToken);
 
-        List<TransplantRequest> potentialTransplantRequests = new ArrayList<>();
-        List<Client> potentialMatches = new ArrayList<>();
-
-        // If the organ trying to be matched has expired, then return an empty list
-        if (donatedOrgan.hasExpired()) {
-            return new ResponseEntity<>(potentialMatches, HttpStatus.OK);
-        }
-
-        // Create a list of eligible transplant requests
-        for (TransplantRequest transplantRequest : State.getClientManager().getAllCurrentTransplantRequests()) {
-            Client donor = donatedOrgan.getDonor();
-            Client recipient = transplantRequest.getClient();
-
-            if (donatedOrgan.getOrganType().equals(transplantRequest.getRequestedOrgan())
-                    && donor.getBloodType().equals(recipient.getBloodType())
-                    && agesMatch(donor.getAge(), recipient.getAge())) {
-                potentialTransplantRequests.add(transplantRequest);
-            }
-        }
+        List<Client> potentialMatches = MatchOrganToRecipients.getListOfPotentialRecipients(donatedOrgan);
 
         return new ResponseEntity<>(potentialMatches, HttpStatus.OK);
 
