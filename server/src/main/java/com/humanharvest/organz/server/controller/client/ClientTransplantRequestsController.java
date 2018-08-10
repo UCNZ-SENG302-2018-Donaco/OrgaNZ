1c0d71fe2ed7fe7de9c84f3247bbd777b59c7ffa
Thomas Kearsley <tke29@uclive.ac.nz>
2018-08-10 14:52:44 +1200

    Updated Organs to Donate FXML #implementation #story[957]

--- server/src/main/java/com/humanharvest/organz/server/controller/client/ClientTransplantRequestsController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/client/ClientTransplantRequestsController.java
@@ -25,6 +25,7 @@ import com.humanharvest.organz.views.client.Views;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
