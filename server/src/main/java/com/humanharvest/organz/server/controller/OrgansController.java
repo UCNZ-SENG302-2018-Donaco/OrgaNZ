fec0669d263d02f6b685003a73618e89d9236750
Alex Tompkins <ato47@uclive.ac.nz>
2018-08-13 11:18:25 +1200

    Implement server endpoints for DELETE /clients/{uid}/donatedOrgans/{id}/override (cancelling a manual override) and PATCH /clients/{uid}/donatedOrgans/{id}/override (editing a manual override) #story[955] #implememt #document !task[m]
    Took 11 minutes

--- server/src/main/java/com/humanharvest/organz/server/controller/OrgansController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/OrgansController.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.humanharvest.organz.Client;
 import com.humanharvest.organz.DonatedOrgan;
 import com.humanharvest.organz.actions.Action;
+import com.humanharvest.organz.actions.client.CancelManualOverrideAction;
+import com.humanharvest.organz.actions.client.EditManualOverrideAction;
 import com.humanharvest.organz.actions.client.ManuallyOverrideOrganAction;
 import com.humanharvest.organz.server.exceptions.GlobalControllerExceptionHandler;
 import com.humanharvest.organz.state.State;
@@ -17,7 +19,9 @@ import com.humanharvest.organz.views.client.Views;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -86,6 +90,7 @@ public class OrgansController {
      * POST endpoint for manually overriding an available organ.
      * @param uid The UID of the client the organ was donated by.
      * @param id The ID of the available organ.
+     * @param overrideReason The reason to override this organ.
      * @param authToken Authentication token - only clinicians and administrators can override available organs.
      * @return Response entity containing the overriden organ.
      * @throws GlobalControllerExceptionHandler.InvalidRequestException If the organ has already been overriden, or if
@@ -131,6 +136,112 @@ public class OrgansController {
                 .orElseThrow(IllegalStateException::new);
 
         // Return the now overriden version of the donated organ along with the client's new ETag.
+        HttpHeaders headers = new HttpHeaders();
+        headers.setETag(client.getETag());
+        return new ResponseEntity<>(donatedOrgan, headers, HttpStatus.OK);
+    }
+
+    /**
+     * DELETE endpoint for cancelling a manual override on an available organ.
+     * @param uid The UID of the client the organ was donated by.
+     * @param id The ID of the available organ.
+     * @param authToken Authentication token - only clinicians and administrators can override available organs.
+     * @return Response entity containing the overriden organ.
+     * @throws GlobalControllerExceptionHandler.InvalidRequestException If the organ has not yet been overriden.
+     */
+    @JsonView(Views.Details.class)
+    @DeleteMapping("/clients/{uid}/donatedOrgans/{id}/override")
+    public ResponseEntity<DonatedOrgan> cancelManualOverride(
+            @PathVariable int uid,
+            @PathVariable int id,
+            @RequestHeader(value = "X-Auth-Token", required = false) String authToken)
+            throws GlobalControllerExceptionHandler.InvalidRequestException {
+
+        // Check that the request is by an authorised user.
+        State.getAuthenticationManager().verifyClinicianOrAdmin(authToken);
+
+        // Check that the UID corresponds to an existing client.
+        Client client = State.getClientManager().getClientByID(uid).orElse(null);
+        if (client == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // Check that the UID corresponds to an existing donated organ.
+        DonatedOrgan donatedOrgan = client.getDonatedOrganById(id);
+        if (donatedOrgan == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // The organ in question must have been overriden.
+        if (donatedOrgan.getOverrideReason() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Create the action and execute it
+        Action action = new CancelManualOverrideAction(donatedOrgan, State.getClientManager());
+        State.getActionInvoker(authToken).execute(action);
+
+        // Retrieve client again (has to be done due to some weird caching issues).
+        client = State.getClientManager()
+                .getClientByID(client.getUid())
+                .orElseThrow(IllegalStateException::new);
+
+        // Return the no longer overriden version of the donated organ along with the client's new ETag.
+        HttpHeaders headers = new HttpHeaders();
+        headers.setETag(client.getETag());
+        return new ResponseEntity<>(donatedOrgan, headers, HttpStatus.OK);
+    }
+
+    /**
+     * PATCH endpoint for manually overriding an available organ.
+     * @param uid The UID of the client the organ was donated by.
+     * @param id The ID of the available organ.
+     * @param newOverrideReason The new reason to override this organ.
+     * @param authToken Authentication token - only clinicians and administrators can override available organs.
+     * @return Response entity containing the overriden organ.
+     * @throws GlobalControllerExceptionHandler.InvalidRequestException If the organ has already been overriden, or if
+     * the reason given is blank.
+     */
+    @JsonView(Views.Details.class)
+    @PatchMapping("/clients/{uid}/donatedOrgans/{id}/override")
+    public ResponseEntity<DonatedOrgan> editManualOverride(
+            @PathVariable int uid,
+            @PathVariable int id,
+            @RequestBody SingleStringView newOverrideReason,
+            @RequestHeader(value = "X-Auth-Token", required = false) String authToken)
+            throws GlobalControllerExceptionHandler.InvalidRequestException {
+
+        // Check that the request is by an authorised user.
+        State.getAuthenticationManager().verifyClinicianOrAdmin(authToken);
+
+        // Check that the UID corresponds to an existing client.
+        Client client = State.getClientManager().getClientByID(uid).orElse(null);
+        if (client == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // Check that the UID corresponds to an existing donated organ.
+        DonatedOrgan donatedOrgan = client.getDonatedOrganById(id);
+        if (donatedOrgan == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // The organ in question must have been overriden.
+        if (donatedOrgan.getOverrideReason() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Create the action and execute it
+        Action action = new EditManualOverrideAction(donatedOrgan, newOverrideReason.getValue(),
+                State.getClientManager());
+        State.getActionInvoker(authToken).execute(action);
+
+        // Retrieve client again (has to be done due to some weird caching issues).
+        client = State.getClientManager()
+                .getClientByID(client.getUid())
+                .orElseThrow(IllegalStateException::new);
+
+        // Return the new version of the overriden donated organ along with the client's new ETag.
         HttpHeaders headers = new HttpHeaders();
         headers.setETag(client.getETag());
         return new ResponseEntity<>(donatedOrgan, headers, HttpStatus.OK);
