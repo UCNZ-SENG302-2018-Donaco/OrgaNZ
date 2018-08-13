aeaef8be283b0cd1cd6c7ce1e01a1198c0b70305
Ollie Chick <och26@uclive.ac.nz>
2018-08-14 02:13:47 +1200

    add tooltip to date of death explaining why it's disabled for manual override #story[959]

--- client/src/main/java/com/humanharvest/organz/controller/client/ViewClientController.java
+++ client/src/main/java/com/humanharvest/organz/controller/client/ViewClientController.java
@@ -31,6 +31,7 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
@@ -98,6 +99,8 @@ public class ViewClientController extends ViewBaseController {
     private ToggleGroup isDeadToggleGroup;
     @FXML
     private ToggleButton aliveToggleBtn, deadToggleBtn;
+    @FXML
+    private GridPane dateOfDeathPane;
 
     public ViewClientController() {
         manager = State.getClientManager();
@@ -197,13 +200,11 @@ public class ViewClientController extends ViewBaseController {
                 aliveToggleBtn.setDisable(true);
                 deadToggleBtn.setDisable(true);
                 deathDatePicker.setDisable(true);
-                // todo put the tooltip in a javafx wrapper (ie some kind of pane), as disabled nodes don't register
-                // mouse events
-                deathDatePicker.setTooltip(new Tooltip("Date of death is not editable because at least one organ has been "
-                        + "manually overridden."));
+                Tooltip tooltip = new Tooltip(
+                        "Date of death is not editable because at least one organ has been manually overridden.");
+                Tooltip.install(dateOfDeathPane, tooltip);
             }
         }
-
 
         // Run these to reset all labels to correct colours
         checkMandatoryFields();
