db73ee402d51c68cedde6df63c38972ef4081d4e
Ollie Chick <och26@uclive.ac.nz>
2018-08-14 23:44:49 +1200

    fix conflicts from merge #story[957]

--- client/src/main/java/com/humanharvest/organz/state/ClientManagerRest.java
+++ client/src/main/java/com/humanharvest/organz/state/ClientManagerRest.java
@@ -9,6 +9,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
