d2525dea6ff2ff7cd14f6e82b3ac2ddbe251de55
Jack Steel <jes143@uclive.ac.nz>
2018-08-15 13:35:58 +1200

    Finally fixed the CheckComboBox to work with TuioFX multi user
    Needed to add a check to MTComboBoxListViewSkin to check if it was a CheckComboBox.
    There may be a more elegant way but this works well.
    
    #fix #story[1054]

---
+++ client/src/main/java/com/humanharvest/organz/utilities/view/MTComboBoxListViewSkin.java
@@ -0,0 +1,122 @@
+package com.humanharvest.organz.utilities.view;
+
+import com.sun.javafx.scene.control.behavior.ComboBoxBaseBehavior;
+import com.sun.javafx.scene.control.skin.ComboBoxListViewSkin;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.BooleanPropertyBase;
+import javafx.css.PseudoClass;
+import javafx.event.EventTarget;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ComboBoxBase;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
+import javafx.scene.transform.Rotate;
+import org.tuiofx.widgets.utils.Util;
+
+public class MTComboBoxListViewSkin<T> extends ComboBoxListViewSkin<T> {
+
+    private static final PseudoClass PRESSED_PSEUDO_CLASS = PseudoClass.getPseudoClass("pressed");
+    private boolean isShowing = false;
+    private BooleanProperty touchPressed = new BooleanPropertyBase(false) {
+        protected void invalidated() {
+            getPopupContent().pseudoClassStateChanged(PRESSED_PSEUDO_CLASS, get());
+        }
+
+        public Object getBean() {
+            return this;
+        }
+
+        public String getName() {
+            return "pressed";
+        }
+    };
+
+    public MTComboBoxListViewSkin(ComboBox<T> comboBox) {
+        super(comboBox);
+
+        getPopup().setAutoHide(false);
+        arrowButton.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> {
+            if (event.isSynthesized()) {
+                getBehavior().mouseEntered(event);
+            }
+
+        });
+        getPopupContent().focusedProperty().addListener((observable, oldValue, newValue) -> {
+            Node owner = getSkinnable();
+            double offsetY = getSkinnable().prefHeight(-1.0D);
+            double angle = Util.getRotationDegreesLocalToScene(owner);
+            getPopupContent().getTransforms().setAll(new Rotate(angle));
+            Rotate rotate = new Rotate(angle);
+            Point2D transformedPoint = rotate.transform(0.0D, offsetY);
+            double popupTopLeftX = owner.getLocalToSceneTransform().getTx();
+            double popupTopLeftY = owner.getLocalToSceneTransform().getTy();
+            double anchorX = popupTopLeftX + transformedPoint.getX() + Util.getOffsetX(owner);
+            double anchorY = popupTopLeftY + transformedPoint.getY() + Util.getOffsetY(owner);
+            getPopup().setAnchorX(anchorX);
+            getPopup().setAnchorY(anchorY);
+        });
+        final ComboBoxBase<T> comboBoxBase = getSkinnable();
+        Node focusAreaNode = Util.getFocusAreaStartingNode(comboBoxBase);
+        if (focusAreaNode != null) {
+            focusAreaNode.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
+                if (!event.isSynthesized() && !isComboBoxOrButton(event.getTarget(), comboBoxBase)) {
+                    handleAutoHidingEvents();
+                }
+
+            });
+            focusAreaNode.addEventFilter(TouchEvent.TOUCH_PRESSED, event -> {
+                if (!isComboBoxOrButton(event.getTarget(), comboBoxBase)) {
+                    handleAutoHidingEvents();
+                }
+
+            });
+            comboBoxBase.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> {
+                if (!event.isSynthesized() && isComboBoxOrButton(event.getTarget(), comboBoxBase) && isShowing) {
+                    handleAutoHidingEvents();
+                    isShowing = false;
+                } else {
+                    isShowing = true;
+                }
+
+            });
+            ((ComboBoxBase) ((ComboBoxBaseBehavior) getBehavior()).getControl()).showingProperty().addListener((observable, oldValue, newValue) -> {
+                if (!newValue) {
+                    getPopup().setAutoHide(false);
+                }
+
+            });
+            comboBox.getScene().getWindow().focusedProperty().addListener((observable, oldValue, newValue) -> {
+                if (!newValue) {
+                    handleAutoHidingEvents();
+                }
+
+            });
+            getPopupContent().addEventHandler(TouchEvent.TOUCH_PRESSED, event -> touchPressed.setValue(true));
+        }
+    }
+
+    private boolean isComboBoxOrButton(EventTarget target, ComboBoxBase<T> comboBoxBase) {
+        return target instanceof Node && "arrow-button".equals(((Node) target).getId()) || comboBoxBase.equals(target);
+    }
+
+    private void handleAutoHidingEvents() {
+        if (getSkinnable().isShowing()) {
+            getPopup().hide();
+            getSkinnable().hide();
+            isShowing = false;
+        }
+
+    }
+
+    /**
+     * This is the main check, we need to see if this is a CheckComboBox and if so do not minimize.
+     *
+     * @return A boolean indicating if clicking an element should close the popup
+     */
+    @Override
+    protected boolean isHideOnClickEnabled() {
+        return !getSkinnable().toString().contains("CheckComboBox");
+    }
+}
