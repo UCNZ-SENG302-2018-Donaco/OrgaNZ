380f3dcadcae596486291f8a9f8fd61459e4836d
Jack Steel <jes143@uclive.ac.nz>
2018-08-16 11:26:05 +1200

    Added fix for some items not being rotated due to custom skin
    #fix #story[1054]

--- client/src/main/java/com/humanharvest/organz/skin/MTContextMenuSkin.java
+++ client/src/main/java/com/humanharvest/organz/skin/MTContextMenuSkin.java
@@ -1,20 +1,23 @@
 package com.humanharvest.organz.skin;
 
-import java.util.Objects;
-import java.util.Optional;
-
+import com.humanharvest.organz.MultitouchHandler;
+import com.sun.javafx.scene.control.skin.ContextMenuContent;
+import com.sun.javafx.scene.control.skin.ContextMenuSkin;
 import javafx.event.Event;
 import javafx.event.EventTarget;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TouchEvent;
-
-import com.humanharvest.organz.MultitouchHandler;
-import com.sun.javafx.scene.control.skin.ContextMenuSkin;
+import javafx.scene.transform.Rotate;
 import org.tuiofx.widgets.utils.Util;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public class MTContextMenuSkin extends ContextMenuSkin {
     private final ContextMenu popupMenu;
@@ -31,6 +34,44 @@ public class MTContextMenuSkin extends ContextMenuSkin {
                 addOwnerNodeHandlers(popupMenu.getOwnerNode());
             });
         }
+
+        popupMenu.addEventHandler(Menu.ON_SHOWN, event -> {
+            Node cmContent = popupMenu.getSkin().getNode();
+            if (cmContent != null || !popupMenu.getStyleClass().contains("text-input-context-menu")) {
+
+                Node owner = getSkinnable().getOwnerNode();
+                double angle = Util.getRotationDegreesLocalToScene(owner);
+                double offsetY = 0.0D;
+                if (cmContent instanceof ContextMenuContent) {
+                    offsetY = getMenuYOffset(cmContent) - 5.0D;
+                }
+
+                if ((int) Math.abs(angle) == 0 || cmContent == null) {
+                    return;
+                }
+
+                cmContent.getTransforms().setAll(new Rotate(angle));
+                Rotate rotate = new Rotate(angle);
+                Point2D transformedPoint = rotate.transform(0.0D, offsetY);
+                double popupTopLeftX = owner.getLocalToSceneTransform().getTx();
+                double popupTopLeftY = owner.getLocalToSceneTransform().getTy();
+                double anchorX = popupTopLeftX + transformedPoint.getX() + Util.getOffsetX(owner);
+                double anchorY = popupTopLeftY + transformedPoint.getY() + Util.getOffsetY(owner);
+                popupMenu.setAnchorX(anchorX);
+                popupMenu.setAnchorY(anchorY);
+            }
+        });
+    }
+
+    private double getMenuYOffset(Node cmContent) {
+        ContextMenuContent contextContent = (ContextMenuContent) cmContent;
+        double offset = 0.0D;
+        if (contextContent.getItemsContainer().getChildren().size() > 0) {
+            Node menuitem = contextContent.getItemsContainer().getChildren().get(0);
+            offset = contextContent.snappedTopInset() + menuitem.getLayoutY() + menuitem.prefHeight(-1.0D);
+        }
+
+        return offset;
     }
 
     private void addOwnerNodeHandlers(Node owner) {
@@ -62,7 +103,7 @@ public class MTContextMenuSkin extends ContextMenuSkin {
     private void handleAutoHidingEvents(EventTarget eventTarget) {
         Node owner = popupMenu.getOwnerNode();
         if (eventTarget != null && owner instanceof ChoiceBox) {
-            Node targetNode = (Node)eventTarget;
+            Node targetNode = (Node) eventTarget;
             if (isPressingChoiceBoxButton(targetNode)) {
                 return;
             }
@@ -70,7 +111,7 @@ public class MTContextMenuSkin extends ContextMenuSkin {
 
         popupMenu.hide();
         if (owner instanceof ChoiceBox) {
-            ((ChoiceBox<?>)owner).hide();
+            ((ChoiceBox<?>) owner).hide();
         }
     }
 
@@ -79,12 +120,11 @@ public class MTContextMenuSkin extends ContextMenuSkin {
         if (Objects.equals(targetNode, owner)) {
             return true;
         } else {
-            for(Parent parent = targetNode.getParent(); parent != null; parent = parent.getParent()) {
+            for (Parent parent = targetNode.getParent(); parent != null; parent = parent.getParent()) {
                 if (Objects.equals(owner, parent)) {
                     return true;
                 }
             }
-
             return false;
         }
     }
