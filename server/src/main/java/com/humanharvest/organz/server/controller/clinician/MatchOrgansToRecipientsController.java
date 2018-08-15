c991d418c1a9e103d24a2b97517d904cc63648da
Ollie <och26@uclive.ac.nz>
2018-08-15 13:30:20 +1200

    Add error checking to server endpoint for matching organs to recipients (including a validator for DonatedOrgan), and updated API spec #story[959]

--- server/src/main/java/com/humanharvest/organz/server/controller/clinician/MatchOrgansToRecipientsController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/clinician/MatchOrgansToRecipientsController.java
@@ -1,19 +1,18 @@
 package com.humanharvest.organz.server.controller.clinician;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.humanharvest.organz.Client;
 import com.humanharvest.organz.DonatedOrgan;
-import com.humanharvest.organz.TransplantRequest;
 import com.humanharvest.organz.state.State;
+import com.humanharvest.organz.utilities.algorithms.MatchOrganToRecipients;
 import com.humanharvest.organz.utilities.exceptions.AuthenticationException;
+import com.humanharvest.organz.utilities.validators.client.DonatedOrganValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.humanharvest.organz.utilities.algorithms.MatchOrganToRecipients;
+
+import java.util.List;
 
 /**
  * Provides handlers for requests to these endpoints:
@@ -24,8 +23,9 @@ public class MatchOrgansToRecipientsController {
     /**
      * Retrieves a list of all clients who are potential recipients of the organ passed in.
      * They are sorted from most to least eligble, with most eligible being the first item in the list.
+     *
      * @param donatedOrgan The organ being donated.
-     * @param authToken The authentication token for the request.
+     * @param authToken    The authentication token for the request.
      * @return HTTP response with a JSON body representing the potential recipients.
      * @throws AuthenticationException If the auth token does not belong to a clinician/admin.
      */
@@ -35,8 +35,13 @@ public class MatchOrgansToRecipientsController {
             @RequestHeader(value = "X-Auth-Token", required = false) String authToken)
             throws AuthenticationException {
 
-        // Verify that request has clinician/admin authorization
+        // Verify that request has clinician/admin authorization - otherwise 401 Unauthorised
         State.getAuthenticationManager().verifyClinicianOrAdmin(authToken);
+
+        // Check that the donated organ is valid - otherwise 400 bad request
+        if (!DonatedOrganValidator.isValid(donatedOrgan)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
         List<Client> potentialMatches = MatchOrganToRecipients.getListOfPotentialRecipients(donatedOrgan);
 
