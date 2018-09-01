d43b0d2e1183a2a85ed1b251c255638622eebfd3
Alex Tompkins <ato47@uclive.ac.nz>
2018-09-01 12:38:48 +1200

    Implement ScheduleTransplantAction #implement #document #story[1143] !task[ea]
    Took 27 minutes

---
+++ server/src/main/java/com/humanharvest/organz/actions/client/procedure/ScheduleTransplantAction.java
@@ -0,0 +1,36 @@
+package com.humanharvest.organz.actions.client.procedure;
+
+import java.time.LocalDate;
+
+import com.humanharvest.organz.DonatedOrgan;
+import com.humanharvest.organz.TransplantRecord;
+import com.humanharvest.organz.TransplantRequest;
+import com.humanharvest.organz.state.ClientManager;
+import com.humanharvest.organz.utilities.exceptions.DateOutOfBoundsException;
+
+/**
+ * A reversible action that will schedule a {@link TransplantRecord} and add it to the
+ * {@link com.humanharvest.organz.ProcedureRecord}s of the recipient.
+ */
+public class ScheduleTransplantAction extends AddProcedureRecordAction {
+
+    /**
+     * Creates a new action to schedule a transplant procedure.
+     *
+     * @param organ The {@link DonatedOrgan} that is going to be transplanted.
+     * @param request The {@link TransplantRequest} to be fulfilled.
+     * @param manager The {@link ClientManager} to apply the changes with.
+     */
+    public ScheduleTransplantAction(DonatedOrgan organ, TransplantRequest request, LocalDate scheduledDate,
+            ClientManager manager) throws DateOutOfBoundsException {
+        // Create a new TransplantRecord using the parameters and pass to AddProcedureRecord constructor
+        super(request.getClient(),
+                new TransplantRecord(organ, request, scheduledDate),
+                manager);
+
+        // Check that date is not out of bounds
+        if (scheduledDate.isBefore(LocalDate.now())) {
+            throw new DateOutOfBoundsException("A transplant cannot be scheduled in the past.");
+        }
+    }
+}
