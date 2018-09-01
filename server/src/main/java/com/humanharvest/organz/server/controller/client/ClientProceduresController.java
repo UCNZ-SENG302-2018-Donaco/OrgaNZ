1dd61bd23000de9c369ee02f6a5f87a6b5e8ac10
Alex Tompkins <ato47@uclive.ac.nz>
2018-09-01 13:02:58 +1200

    Implement POST /clients/{uid}/transplants endpoint -Currently just takes organ, requests ids & date as query parameters, can switch to custom request body instead -Validation for correct client, organ, request ids and that organ hasn't been donated yet/request has not yet been fulfilled, and scheduled date is not in the past #implement #document #story[1143] !task[ga]
    Took 20 minutes

--- server/src/main/java/com/humanharvest/organz/server/controller/client/ClientProceduresController.java
+++ server/src/main/java/com/humanharvest/organz/server/controller/client/ClientProceduresController.java
@@ -2,19 +2,25 @@ package com.humanharvest.organz.server.controller.client;
 
 import static com.humanharvest.organz.utilities.validators.ClientValidator.checkClientETag;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.humanharvest.organz.Client;
+import com.humanharvest.organz.DonatedOrgan;
 import com.humanharvest.organz.ProcedureRecord;
+import com.humanharvest.organz.TransplantRequest;
 import com.humanharvest.organz.actions.Action;
 import com.humanharvest.organz.actions.client.procedure.AddProcedureRecordAction;
 import com.humanharvest.organz.actions.client.procedure.DeleteProcedureRecordAction;
 import com.humanharvest.organz.actions.client.procedure.ModifyProcedureRecordAction;
+import com.humanharvest.organz.actions.client.procedure.ScheduleTransplantAction;
 import com.humanharvest.organz.state.State;
+import com.humanharvest.organz.utilities.enums.TransplantRequestStatus;
 import com.humanharvest.organz.utilities.exceptions.AuthenticationException;
+import com.humanharvest.organz.utilities.exceptions.DateOutOfBoundsException;
 import com.humanharvest.organz.utilities.exceptions.IfMatchFailedException;
 import com.humanharvest.organz.utilities.exceptions.IfMatchRequiredException;
 import com.humanharvest.organz.views.client.ModifyProcedureObject;
@@ -31,6 +37,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -211,5 +218,64 @@ public class ClientProceduresController {
 
         // Return OK response
         return new ResponseEntity<>(headers, HttpStatus.OK);
+    }
+
+    @PostMapping("/clients/{uid}/transplants")
+    public ResponseEntity<Collection<ProcedureRecord>> scheduleTransplantProcedure(
+            @PathVariable int uid,
+            @RequestParam long organId,
+            @RequestParam long requestId,
+            @RequestParam LocalDate date,
+            @RequestHeader(value = "If-Match", required = false) String ETag,
+            @RequestHeader(value = "X-Auth-Token", required = false) String authToken)
+            throws AuthenticationException, IfMatchFailedException, IfMatchRequiredException {
+
+        // Check request has authorization to create a procedure
+        State.getAuthenticationManager().verifyClinicianOrAdmin(authToken);
+
+        Optional<Client> optionalClient = State.getClientManager().getClientByID(uid);
+        if (!optionalClient.isPresent()) {
+            // No client exists with that id, return 404
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Client client = optionalClient.get();
+
+        // Check ETag
+        checkClientETag(client, ETag);
+
+        // Find the organ to be transplanted
+        DonatedOrgan organ = client.getDonatedOrganById(organId);
+        if (organ == null) {
+            // No organ exists with that id, return 400
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Find the recipient's request for this organ
+        Optional<TransplantRequest> optionalRequest = client.getTransplantRequestById(requestId);
+        if (!optionalRequest.isPresent()) {
+            // No request exists with that id, return 400
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        TransplantRequest request = optionalRequest.get();
+
+        // Check that the organ hasn't been donated yet, and the request is still waiting
+        if (!(organ.getReceiver() == null && request.getStatus() == TransplantRequestStatus.WAITING)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Execute add procedure action
+        try {
+            Action action = new ScheduleTransplantAction(organ, request, date, State.getClientManager());
+            State.getActionInvoker(authToken).execute(action);
+        } catch (DateOutOfBoundsException exc) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Add the new ETag to the headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setETag(client.getETag());
+
+        // Return response containing list of client's procedures
+        return new ResponseEntity<>(client.getProcedures(), headers, HttpStatus.CREATED);
     }
 }
