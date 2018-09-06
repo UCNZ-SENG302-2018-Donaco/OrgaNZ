e20b6ce57e2b85fd607f95fe00e5c589728f36bb
james <jto59@uclive.ac.nz>
2018-09-06 19:20:23 +1200

    A new stage now opens for the sipderweb page rather than clearing all currently open panes. #implement #story[1132]

--- client/src/main/java/com/humanharvest/organz/controller/SpiderWebController.java
+++ client/src/main/java/com/humanharvest/organz/controller/SpiderWebController.java
@@ -1,11 +1,20 @@
 package com.humanharvest.organz.controller;
 
+import java.io.IOException;
 import java.util.logging.Logger;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import com.humanharvest.organz.Client;
 import com.humanharvest.organz.DonatedOrgan;
 import com.humanharvest.organz.controller.client.ViewClientController;
+import com.humanharvest.organz.state.State;
 import com.humanharvest.organz.utilities.exceptions.ServerRestException;
+import com.humanharvest.organz.utilities.view.Page;
+import com.humanharvest.organz.utilities.view.PageNavigatorStandard;
 
 public class SpiderWebController extends SubController {
 
@@ -13,11 +22,40 @@ public class SpiderWebController extends SubController {
 
     public SpiderWebController() {
 //        this.client = client;
-        this.client = windowContext.getViewClient();
+        try {
+//            Stage newStage = new Stage();
+//
+//            FXMLLoader loader = new FXMLLoader();
+//            MainController mainController = loader.getController();
+//            mainController.setStage(newStage);
+//            Pane mainPane = loader.load(PageNavigatorStandard.class.getResourceAsStream(Page.MAIN.getPath()));
+//
+//            Scene scene = new Scene(mainPane);
+//            newStage.setScene(scene);
+//            newStage.show();
+//
+//            this.client = windowContext.getViewClient();
+            Stage newStage = new Stage();
+            newStage.setTitle("Organ Client Management System");
+            FXMLLoader loader = new FXMLLoader();
+            Pane mainPane = loader.load(PageNavigatorStandard.class.getResourceAsStream(Page.MAIN.getPath()));
+            MainController mainController = loader.getController();
+            mainController.setStage(newStage);
+            mainController.setPane(mainPane);
+            State.addMainController(mainController);
+            newStage.setOnCloseRequest(e -> State.deleteMainController(mainController));
 
-        //display client in center of page
+            Scene scene = new Scene(mainPane);
+            newStage.setScene(scene);
+            newStage.setFullScreen(true);
+            newStage.show();
 
-        displayOrgans();
+            //display client in center of page
+
+            displayOrgans();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
     }
 
     private void displayOrgans() {
