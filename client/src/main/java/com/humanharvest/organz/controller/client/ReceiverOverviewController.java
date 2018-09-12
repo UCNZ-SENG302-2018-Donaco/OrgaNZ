7770d953778b3502ffe94a5fdd8c68c749b22b05
Thomas Kearsley <tke29@uclive.ac.nz>
2018-09-12 21:29:33 +1200

    Working with liver organ (temporary) provided a valid transplant request exists. Will be changed to all organs when spider web is complete. #implement #story[1129]

--- client/src/main/java/com/humanharvest/organz/controller/client/ReceiverOverviewController.java
+++ client/src/main/java/com/humanharvest/organz/controller/client/ReceiverOverviewController.java
@@ -2,6 +2,8 @@ package com.humanharvest.organz.controller.client;
 
 import static com.humanharvest.organz.utilities.DurationFormatter.getFormattedDuration;
 
+import com.humanharvest.organz.state.ClientManager;
+import com.humanharvest.organz.state.Session;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
@@ -31,118 +33,132 @@ import com.humanharvest.organz.utilities.view.WindowContext.WindowContextBuilder
 
 public class ReceiverOverviewController extends ViewBaseController {
 
-    private static final Logger LOGGER = Logger.getLogger(ReceiverOverviewController.class.getName());
+  private static final Logger LOGGER = Logger.getLogger(ReceiverOverviewController.class.getName());
 
 
-    private Client viewedClient;
-    private TransplantRequest viewedTransplantRequest;
+  private Client viewedClient;
+  private TransplantRequest viewedTransplantRequest;
+  private final Session session;
+  private final ClientManager manager;
 
-    @FXML
-    private ImageView imageView;
 
-    @FXML
-    private Label travelTime;
+  @FXML
+  private ImageView imageView;
 
-    @FXML
-    private Label requestedTime;
+  @FXML
+  private Label travelTime;
 
-    @FXML
-    private Label name;
+  @FXML
+  private Label requestedTime;
 
-    @FXML
-    private Label hospital;
+  @FXML
+  private Label name;
 
-    @FXML
-    private Label age;
+  @FXML
+  private Label hospital;
 
-    @FXML
-    private  VBox receiverVBox;
-    /*
-        public ReceiverOverviewController(Client client, Organ organ) {
-            viewedClient = client;
-            System.out.println(viewedClient.getFullName());
-            System.out.println(viewedClient.getTransplantRequests().size());
-            viewedTransplantRequest = viewedClient.getTransplantRequest(organ);
+  @FXML
+  private Label age;
+
+  @FXML
+  private VBox receiverVBox;
+
+  /*
+      public ReceiverOverviewController(Client client, Organ organ) {
+          viewedClient = client;
+          System.out.println(viewedClient.getFullName());
+          System.out.println(viewedClient.getTransplantRequests().size());
+          viewedTransplantRequest = viewedClient.getTransplantRequest(organ);
+      }
+  */
+  public ReceiverOverviewController() { // test with first client
+    manager = State.getClientManager();
+    session = State.getSession();
+  }
+
+  /**
+   * Initializes the UI for this page.
+   */
+  private void setClientFields() {
+    List<TransplantRequest> transplantRequests = State.getClientResolver()
+        .getTransplantRequests(viewedClient);
+    viewedTransplantRequest = null;
+
+    Organ organ = Organ.LIVER; // for testing
+
+    for (TransplantRequest transplantRequest : transplantRequests) {
+      if (transplantRequest.getRequestedOrgan() == organ) {
+        viewedTransplantRequest = viewedClient.getTransplantRequest(Organ.LIVER);
+      }
+    }
+
+    try {
+      Client reciever = viewedTransplantRequest.getClient();
+      name.setText(reciever.getFullName());
+      hospital.setText(reciever.getHospital().getName());
+      travelTime
+          .setText(viewedClient.getHospital().calculateTimeTo(reciever.getHospital()).toString());
+
+      loadImage();
+
+      // Track the adding of panes with the spiderweb pane collection.
+      receiverVBox.setOnMouseClicked(mouseEvent -> {
+        if (mouseEvent.getButton().equals(MouseButton.PRIMARY) && mouseEvent.getClickCount() == 1) {
+          MainController newMain = PageNavigator.openNewWindow();
+          if (newMain != null) {
+            newMain.setWindowContext(new WindowContextBuilder()
+                .setAsClinicianViewClientWindow()
+                .viewClient(viewedClient)
+                .build());
+            PageNavigator.loadPage(Page.VIEW_CLIENT, newMain);
+          }
         }
-    */
-    public ReceiverOverviewController() { // test with first client
-        viewedClient = State.getClientManager().getClients().get(0);
-        List<TransplantRequest> transplantRequests = State.getClientResolver().getTransplantRequests(viewedClient);
-        System.out.print(viewedClient.getFullName()+": ");
-        System.out.println(viewedClient.getTransplantRequests().size());
-        viewedTransplantRequest = null;
-
-        Organ organ = Organ.LIVER; // for testing
-
-        for (TransplantRequest transplantRequest : transplantRequests) {
-            if (transplantRequest.getRequestedOrgan() == organ) {
-                viewedTransplantRequest = viewedClient.getTransplantRequest(Organ.LIVER);
-            }
-        }
-    }
-
-    /**
-     * Initializes the UI for this page.
-     */
-    @FXML
-    private void initialize() {
-        name.setText(viewedClient.getFullName());
-        loadImage();
-        age.setText(String.valueOf(viewedClient.getAge()));
-        hospital.setText("X Hospital"); //todo
-        travelTime.setText("x hours"); //todo
-        if (viewedTransplantRequest == null) {
-            requestedTime.setText("ERROR: not requested");
-        } else {
-            requestedTime.setText(getFormattedDuration(viewedTransplantRequest.getTimeSinceRequest(), Format.Biggest));
-        }
-
-        // Track the adding of panes with the spiderweb pane collection.
-        receiverVBox.setOnMouseClicked(mouseEvent -> {
-            if (mouseEvent.getButton().equals(MouseButton.PRIMARY) && mouseEvent.getClickCount() == 1) {
-                    MainController newMain = PageNavigator.openNewWindow();
-                    if (newMain != null) {
-                        newMain.setWindowContext(new WindowContextBuilder()
-                                .setAsClinicianViewClientWindow()
-                                .viewClient(viewedClient)
-                                .build());
-                        PageNavigator.loadPage(Page.VIEW_CLIENT, newMain);
-                    }
-                }
-            });
+      });
+    } catch (NullPointerException e) {
+      PageNavigator.showAlert(AlertType.ERROR, "No Reciever","No Reciever that required Liver",mainController.getStage());
 
     }
 
-    @Override
-    public void setup(MainController mainController) {
-        super.setup(mainController);
-        refresh();
-    }
+  }
 
-    @Override
-    public void refresh() {
-        //TODO
-    }
+  @Override
+  public void setup(MainController mainController) {
+    super.setup(mainController);
+    viewedClient = windowContext.getViewClient();
+    setClientFields();
+    refresh();
+  }
 
-    private void loadImage() {
-        byte[] bytes;
-        try {
-            bytes = State.getImageManager().getClientImage(viewedClient.getUid());
-        } catch (NotFoundException ignored) {
-            try {
-                bytes = State.getImageManager().getDefaultImage();
-            } catch (IOException e) {
-                LOGGER.log(Level.SEVERE, "IO Exception when loading image ", e);
-                return;
-            }
-        } catch (ServerRestException e) {
-            PageNavigator.showAlert(AlertType.ERROR, "Server Error", "Something went wrong with the server. "
-                    + "Please try again later.", mainController.getStage());
-            LOGGER.log(Level.SEVERE, e.getMessage(), e);
-            return;
-        }
-        Image image = new Image(new ByteArrayInputStream(bytes));
-        imageView.setImage(image);
+  @Override
+  public void refresh() {
+    //TODO
+  }
+
+  private void loadImage() {
+    byte[] bytes;
+    try {
+      bytes = State.getImageManager().getClientImage(viewedClient.getUid());
+    } catch (NotFoundException ignored) {
+      try {
+        bytes = State.getImageManager().getDefaultImage();
+      } catch (IOException e) {
+        LOGGER.log(Level.SEVERE, "IO Exception when loading image ", e);
+        return;
+      }
+    } catch (ServerRestException e) {
+      PageNavigator
+          .showAlert(AlertType.ERROR, "Server Error", "Something went wrong with the server. "
+              + "Please try again later.", mainController.getStage());
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+      return;
     }
+    Image image = new Image(new ByteArrayInputStream(bytes));
+    imageView.setImage(image);
+  }
+
+  @FXML
+  private void initialize() {
+
+  }
 
 }
