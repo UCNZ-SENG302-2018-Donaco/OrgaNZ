9affb569882e2bb702c6c33825a393782c931a0e
Matthew <mjs351@uclive.ac.nz>
2018-09-21 15:42:10 +1200

    Tweaked physics calculations to reduce slowdown when throwing organ icons around.
    #story[1032]

--- client/src/main/java/com/humanharvest/organz/controller/spiderweb/SpiderPhysicsHandler.java
+++ client/src/main/java/com/humanharvest/organz/controller/spiderweb/SpiderPhysicsHandler.java
@@ -14,8 +14,6 @@ import com.humanharvest.organz.touch.PointUtils;
 public class SpiderPhysicsHandler extends PhysicsHandler {
 
     private static final double MOVE_ASIDE_VELOCITY_PER_TICK = 2;
-    private static final double MAX_MOVE_ASIDE_VELOCITY = 200;
-    private static final double MOVE_ASIDE_DAMPING = 0.95;
 
     public SpiderPhysicsHandler(Pane rootPane) {
         super(rootPane);
@@ -55,15 +53,6 @@ public class SpiderPhysicsHandler extends PhysicsHandler {
                     Point2D otherCentre = PointUtils.getCentreOfBounds(otherBounds);
                     Point2D velocityDelta = centre.subtract(otherCentre).normalize();
                     focusArea.addVelocity(velocityDelta.multiply(MOVE_ASIDE_VELOCITY_PER_TICK));
-
-                    // If the velocity is greater then MAX_MOVE_ASIDE_VELOCITY, dampen the velocity
-                    double length = Math.abs(focusArea.getVelocity().distance(Point2D.ZERO));
-                    if (length > MAX_MOVE_ASIDE_VELOCITY) {
-                        focusArea.setVelocity(focusArea
-                                .getVelocity()
-                                .normalize()
-                                .multiply(length * MOVE_ASIDE_DAMPING));
-                    }
                 }
             }
         }
