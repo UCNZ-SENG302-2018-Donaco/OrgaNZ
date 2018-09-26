474751c82c04e5c7c6be967488c9fe9ad19249c0
Jack Steel <jes143@uclive.ac.nz>
2018-09-27 03:56:20 +1200

    Fix badges not respawning after override
    #fix #story[1133]

--- client/src/main/java/com/humanharvest/organz/controller/spiderweb/OrganWithRecipients.java
+++ client/src/main/java/com/humanharvest/organz/controller/spiderweb/OrganWithRecipients.java
@@ -299,7 +299,7 @@ public class OrganWithRecipients {
             organ.cancelManualOverride();
             donorOverviewController.fetchOrgans();
 
-            drawMatchesPane(false);
+            drawMatchesPane(true);
         }
 
         organImageController.matchCountIsVisible(false);
