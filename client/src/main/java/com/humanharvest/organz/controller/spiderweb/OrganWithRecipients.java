d8ad673569cea5eb78e93e0eb186be4d1713224f
Matthew Smit <programmdude@gmail.com>
2018-09-27 14:50:57 +1200

    ToFront is no longer being called each frame for recipients

--- client/src/main/java/com/humanharvest/organz/controller/spiderweb/OrganWithRecipients.java
+++ client/src/main/java/com/humanharvest/organz/controller/spiderweb/OrganWithRecipients.java
@@ -437,12 +437,6 @@ public class OrganWithRecipients {
 
         setRecipientConnectorStart(bounds);
         setRecipientConnectorEnd(matchesPane.getBoundsInParent());
-
-        try {
-            organPane.toFront();
-        } catch (RuntimeException e) {
-            LOGGER.log(Level.WARNING, "Runtime exception when setting pane to front", e);
-        }
     }
 
     public void handlePotentialMatchesTransformed() {
